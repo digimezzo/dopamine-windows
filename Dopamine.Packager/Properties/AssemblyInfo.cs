4c4f9a77e4291e7b48dbf707d2b950e51a7c339e
Raphaël <raphael.godart@outlook.com>
2016-07-29 23:25:36 +0200

    Added SharedAssemblyInfo, fixed issues following conversion to C#

--- Dopamine.Packager/Properties/AssemblyInfo.cs
+++ Dopamine.Packager/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Dopamine.Packager")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+//[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Dopamine.Packager")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+//[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+//[assembly: AssemblyVersion("1.0.0.0")]
+//[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Dopamine/SharedAssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyCompany("Digimezzo")]
+[assembly: AssemblyCopyright("Copyright Digimezzo © 2014 - 2016")]
+[assembly: AssemblyVersion("1.1.666.0")]
+[assembly: AssemblyFileVersion("1.1.666.0")]
+
+#if DEBUG
+[assembly: AssemblyInformationalVersion("1.1.666.0 Preview")]
+#else
+[assembly: AssemblyInformationalVersion("1.1.666.0")]
+#endif
