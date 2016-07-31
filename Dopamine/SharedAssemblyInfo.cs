e50c0544c5fda380ab68d3711f19863091e9056c
Raphaël <raphael.godart@outlook.com>
2016-07-31 08:28:39 +0200

    Used another Unity.WVF package

--- Dopamine/SharedAssemblyInfo.cs
+++ Dopamine/SharedAssemblyInfo.cs
@@ -2,11 +2,11 @@
 
 [assembly: AssemblyCompany("Digimezzo")]
 [assembly: AssemblyCopyright("Copyright Digimezzo © 2014 - 2016")]
-[assembly: AssemblyVersion("1.1.667.0")]
-[assembly: AssemblyFileVersion("1.1.667.0")]
+[assembly: AssemblyVersion("1.1.668.0")]
+[assembly: AssemblyFileVersion("1.1.668.0")]
 
 #if DEBUG
-[assembly: AssemblyInformationalVersion("1.1.667.0 Preview")]
+[assembly: AssemblyInformationalVersion("1.1.668.0 Preview")]
 #else
-[assembly: AssemblyInformationalVersion("1.1.667.0")]
+[assembly: AssemblyInformationalVersion("1.1.668.0")]
 #endif
--- Dopamine/readme.txt
+++
@@ -1,7 +0,0 @@
-﻿    public class WcfServiceFactory : UnityServiceHostFactory {
-
-        protected override void ConfigureContainer(IUnityContainer container) {
-            container.LoadConfiguration();
-            //UnityConfig.RegisterTypes(container);
-        }
-    }
