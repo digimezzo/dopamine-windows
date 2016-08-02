1222c0412608311e503b4c3eec5bea578c496865
Raphaël <raphael.godart@outlook.com>
2016-08-02 22:35:35 +0200

    Removed obsolete usings

--- Dopamine.Common/Presentation/ViewModels/CoverPlaybackInfoControlViewModel.cs
+++ Dopamine.Common/Presentation/ViewModels/CoverPlaybackInfoControlViewModel.cs
@@ -1,10 +1,5 @@
 ﻿using Dopamine.Common.Services.Playback;
 using Microsoft.Practices.ServiceLocation;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Dopamine.Common.Presentation.ViewModels
 {
