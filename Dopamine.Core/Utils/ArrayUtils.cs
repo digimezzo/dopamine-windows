5cd8da33865d6b965d3dbc39b73429566dea576c
Digimezzo <raphael.godart@outlook.com>
2016-08-09 13:26:05 +0200

    Added ArrayUtils

---
+++ Dopamine.Core/Utils/ArrayUtils.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dopamine.Core.Utils
+{
+    public static class ArrayUtils
+    {
+        public static double[] ConvertArray(string[] array)
+        {
+            return Array.ConvertAll(array, s => double.Parse(s));
+        }
+    }
+}
