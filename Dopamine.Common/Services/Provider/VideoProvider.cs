3bc2e0dc851077b717109944fe528cba607b2198
Raphaël <raphael.godart@outlook.com>
2016-08-30 07:38:09 +0200

    Started implementation of video providers

---
+++ Dopamine.Common/Services/Provider/VideoProvider.cs
@@ -0,0 +1,9 @@
+﻿namespace Dopamine.Common.Services.Provider
+{
+    public class VideoProvider
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+        public string Separator { get; set; }
+    }
+}
