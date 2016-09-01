7d71fb07ba3aebc30c663ccec058b8ad72a07390
gjhwssg <gjhwssg@gmail.com>
2016-09-01 22:13:42 +0800

    Fixes #51 :Bluetooth headset play/pause button not detected

--- Dopamine.Common/Services/Win32Input/IWin32InputService.cs
+++ Dopamine.Common/Services/Win32Input/IWin32InputService.cs
@@ -4,7 +4,7 @@ namespace Dopamine.Common.Services.Win32Input
 {
     public interface IWin32InputService
     {
-        void SetKeyboardHook();
+        void SetKeyboardHook(IntPtr hwnd);
         void UnhookKeyboard();
         event EventHandler MediaKeyNextPressed;
         event EventHandler MediaKeyPreviousPressed;
