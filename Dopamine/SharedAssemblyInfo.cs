a49909df36b341e39c70befc226839626698644b
Raphaël <raphael.godart@outlook.com>
2016-09-12 23:32:58 +0200

    Updated the Chinese Traditional translation

--- Dopamine/SharedAssemblyInfo.cs
+++ Dopamine/SharedAssemblyInfo.cs
@@ -2,11 +2,11 @@
 
 [assembly: AssemblyCompany("Digimezzo")]
 [assembly: AssemblyCopyright("Copyright Digimezzo © 2014 - 2016")]
-[assembly: AssemblyVersion("1.1.714.2")]
-[assembly: AssemblyFileVersion("1.1.714.2")]
+[assembly: AssemblyVersion("1.2.715.0")]
+[assembly: AssemblyFileVersion("1.2.715.0")]
 
 #if DEBUG
-[assembly: AssemblyInformationalVersion("1.1.714.2 Preview")]
+[assembly: AssemblyInformationalVersion("1.2.715.0 Preview")]
 #else
-[assembly: AssemblyInformationalVersion("1.1.714.2")]
+[assembly: AssemblyInformationalVersion("1.2.715.0")]
 #endif
