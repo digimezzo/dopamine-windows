03619b14b4d0342fb22ec3d55cce310f40cc31cd
Raphaël <raphael.godart@outlook.com>
2016-09-21 20:46:14 +0200

    Tweaked playback info pane

--- Dopamine/SharedAssemblyInfo.cs
+++ Dopamine/SharedAssemblyInfo.cs
@@ -2,11 +2,11 @@
 
 [assembly: AssemblyCompany("Digimezzo")]
 [assembly: AssemblyCopyright("Copyright Digimezzo © 2014 - 2016")]
-[assembly: AssemblyVersion("1.2.726.0")]
-[assembly: AssemblyFileVersion("1.2.726.0")]
+[assembly: AssemblyVersion("1.2.728.0")]
+[assembly: AssemblyFileVersion("1.2.728.0")]
 
 #if DEBUG
-[assembly: AssemblyInformationalVersion("1.2.726.0 Preview")]
+[assembly: AssemblyInformationalVersion("1.2.728.0 Preview")]
 #else
-[assembly: AssemblyInformationalVersion("1.2.726.0")]
+[assembly: AssemblyInformationalVersion("1.2.728.0")]
 #endif
