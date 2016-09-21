8c662c297661fb6b29d7f34bed20fd825a554117
Raphaël <raphael.godart@outlook.com>
2016-09-21 20:46:14 +0200

    Tweaked playback info pane

--- Dopamine.ControlsModule/Views/CollectionPlaybackControls.xaml.cs
+++ Dopamine.ControlsModule/Views/CollectionPlaybackControls.xaml.cs
@@ -13,27 +13,6 @@ namespace Dopamine.ControlsModule.Views
         }
         #endregion
 
-        private void PlaybackInfoPanel_SizeChanged(object sender, System.Windows.SizeChangedEventArgs e)
-        {
-            this.SetPlaybackInfoPanelVisibility();
-        }
-
-        private void SetPlaybackInfoPanelVisibility()
-        {
-            if (this.PlaybackInfoPanel.ActualWidth >= 250)
-            {
-                this.CoverArtControl.Visibility = Visibility.Visible;
-                this.CoverArtControl.Margin = new Thickness(0, 0, 10, 0);
-            }
-            else
-            {
-                // Using Visibility.Colapsed is not an option. It causes CoverArtControl
-                // to remain invisible after calling Visibility.Visible.
-                this.CoverArtControl.Visibility = Visibility.Hidden;
-                this.CoverArtControl.Margin = new Thickness(-this.CoverArtControl.ActualWidth - 2, 0, 2, 0);
-            }
-        }
-
         private void SetTotalTimeVisibility()
         {
             if (this.TimeBorder.ActualWidth >= 90)
