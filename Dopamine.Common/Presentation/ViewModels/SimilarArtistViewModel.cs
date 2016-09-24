a2fd2d29c9c7a5c1194aa318e13a5e60d9752034
Raphaël <raphael.godart@outlook.com>
2016-09-24 23:26:00 +0200

    Fixed a nullreferenceexception

---
+++ Dopamine.Common/Presentation/ViewModels/SimilarArtistViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Prism.Mvvm;
+
+namespace Dopamine.Common.Presentation.ViewModels
+{
+    //public class SimilarArtistViewModel : BindableBase
+    //{
+    //    #region Private
+    //    private string name;
+    //    private string url;
+    //    #endregion
+
+    //    #region Properties
+    //    public string Name
+    //    {
+    //        get { return this.name; }
+    //        set { SetProperty<string>(ref this.name, value); }
+    //    }
+
+    //    public string Url
+    //    {
+    //        get { return this.url; }
+    //        set { SetProperty<string>(ref this.url, value); }
+    //    }
+    //    #endregion
+    //}
+}
