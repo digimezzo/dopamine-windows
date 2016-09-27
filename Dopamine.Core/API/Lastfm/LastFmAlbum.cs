b5ce292cbc0b805d85e93d53db0870d09fc87bd6
Raphaël <raphael.godart@outlook.com>
2016-09-27 18:10:27 +0200

    Added AlbumGetInfo function to LastFm API

---
+++ Dopamine.Core/API/Lastfm/LastFmAlbum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Dopamine.Core.API.Lastfm
+{
+    public class LastFmAlbum
+    {
+        public string Name { get; set; }
+        public string Artist { get; set; }
+        public string Url { get; set; }
+        public string ImageSmall { get; set; }
+        public string ImageMedium { get; set; }
+        public string ImageLarge { get; set; }
+        public string ImageExtraLarge { get; set; }
+        public string ImageMega { get; set; }
+    }
+}
