040d6c06d18ca0523b72ca3cfd0fbc671647e145
Raphaël <raphael.godart@outlook.com>
2016-09-29 22:53:23 +0200

    Revert "Added properties for the checkboxes"
    This reverts commit 1bf732fd9f6c06822bae3e4903356f94da4228c2.

--- Dopamine.SettingsModule/ViewModels/SettingsOnlineDownloadAlbumCoversViewModel.cs
+++ Dopamine.SettingsModule/ViewModels/SettingsOnlineDownloadAlbumCoversViewModel.cs
@@ -1,54 +1,6 @@
-﻿using Dopamine.Core.Settings;
-using Prism.Mvvm;
-using System.Threading.Tasks;
-namespace Dopamine.SettingsModule.ViewModels
+﻿namespace Dopamine.SettingsModule.ViewModels
 {
-    public class SettingsOnlineDownloadAlbumCoversViewModel : BindableBase
+    public class SettingsOnlineDownloadAlbumCoversViewModel
     {
-        #region Variables
-        private bool checkBoxDownloadMissingAlbumCoversChecked;
-        private bool checkBoxEmbedDownloadedCoversInAudioFilesChecked;
-        #endregion
-
-        #region Properties
-        public bool CheckBoxDownloadMissingAlbumCoversChecked
-        {
-            get { return this.checkBoxDownloadMissingAlbumCoversChecked; }
-            set
-            {
-                XmlSettingsClient.Instance.Set<bool>("Lastfm", "DownloadAlbumCovers", value);
-                SetProperty<bool>(ref this.checkBoxDownloadMissingAlbumCoversChecked, value);
-            }
-        }
-
-        public bool CheckBoxEmbedDownloadedCoversInAudioFilesChecked
-        {
-            get { return this.checkBoxEmbedDownloadedCoversInAudioFilesChecked; }
-            set
-            {
-                XmlSettingsClient.Instance.Set<bool>("Lastfm", "EmbedAlbumCoversInAudioFiles", value);
-                SetProperty<bool>(ref this.checkBoxEmbedDownloadedCoversInAudioFilesChecked, value);
-            }
-        }
-        #endregion
-
-        #region Construction
-        public SettingsOnlineDownloadAlbumCoversViewModel()
-        {
-            this.GetCheckBoxesAsync();
-        }
-        #endregion
-
-        #region Private
-        private async void GetCheckBoxesAsync()
-        {
-            await Task.Run(() =>
-            {
-                this.CheckBoxDownloadMissingAlbumCoversChecked = XmlSettingsClient.Instance.Get<bool>("Lastfm", "DownloadAlbumCovers");
-                this.CheckBoxEmbedDownloadedCoversInAudioFilesChecked = XmlSettingsClient.Instance.Get<bool>("Lastfm", "EmbedAlbumCoversInAudioFiles");
-
-            });
-        }
-        #endregion
     }
 }
