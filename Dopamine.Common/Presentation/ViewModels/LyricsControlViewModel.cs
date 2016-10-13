09928df29e6065423aa36fa81de0efd028668b2c
Raphaël <raphael.godart@outlook.com>
2016-10-13 20:12:38 +0200

    Building the lyrics screen

--- Dopamine.Common/Presentation/ViewModels/LyricsControlViewModel.cs
+++ Dopamine.Common/Presentation/ViewModels/LyricsControlViewModel.cs
@@ -1,9 +1,13 @@
-﻿using System;
+﻿using Digimezzo.WPFControls.Enums;
+using Dopamine.Common.Services.I18n;
 using Dopamine.Common.Services.Playback;
-using Prism.Mvvm;
 using Dopamine.Core.Database;
-using System.Threading.Tasks;
+using Dopamine.Core.Logging;
 using Dopamine.Core.Metadata;
+using Dopamine.Core.Utils;
+using Prism.Mvvm;
+using System;
+using System.Threading.Tasks;
 
 namespace Dopamine.Common.Presentation.ViewModels
 {
@@ -11,36 +15,53 @@ namespace Dopamine.Common.Presentation.ViewModels
     {
         #region Variables
         private IPlaybackService playbackService;
+        private II18nService i18nService;
         private string title;
         private string staticLyrics;
+        private LyricsViewModel lyricsViewModel;
+        private SlideDirection slideDirection;
+        private TrackInfo previousTrackInfo;
+        private TrackInfo trackInfo;
         #endregion
 
         #region Properties
-        public string Title
+        public SlideDirection SlideDirection
         {
-            get { return this.title; }
-            set { SetProperty<string>(ref this.title, value); }
+            get { return this.slideDirection; }
+            set { SetProperty<SlideDirection>(ref this.slideDirection, value); }
         }
 
-        public string StaticLyrics
+        public LyricsViewModel LyricsViewModel
         {
-            get { return this.staticLyrics; }
-            set { SetProperty<string>(ref this.staticLyrics, value); }
+            get { return this.lyricsViewModel; }
+            set { SetProperty<LyricsViewModel>(ref this.lyricsViewModel, value); }
         }
         #endregion
 
         #region Construction
-        public LyricsControlViewModel(IPlaybackService playbackService)
+        public LyricsControlViewModel(IPlaybackService playbackService, II18nService i18nService)
         {
             this.playbackService = playbackService;
+            this.i18nService = i18nService;
 
             this.playbackService.PlaybackFailed += (_, __) => this.ShowLyricsAsync(null);
             this.playbackService.PlaybackStopped += (_, __) => this.ShowLyricsAsync(null);
 
             this.playbackService.PlaybackSuccess += (isPlayingPreviousTrack) =>
             {
+                if (isPlayingPreviousTrack)
+                {
+                    this.SlideDirection = SlideDirection.UpToDown;
+                }
+                else
+                {
+                    this.SlideDirection = SlideDirection.DownToUp;
+                }
+
                 this.ShowLyricsAsync(this.playbackService.PlayingTrack);
             };
+
+            this.i18nService.LanguageChanged += (_, __) => this.ShowLyricsAsync(this.playbackService.PlayingTrack);
 
             this.ShowLyricsAsync(this.playbackService.PlayingTrack);
         }
@@ -49,15 +70,45 @@ namespace Dopamine.Common.Presentation.ViewModels
         #region Private
         private async void ShowLyricsAsync(TrackInfo trackInfo)
         {
-            if (trackInfo == null) return;
+            this.previousTrackInfo = this.trackInfo;
 
-            await Task.Run(() =>
+            // No track selected: clear playback info.
+            if (trackInfo == null)
+            {
+                this.LyricsViewModel = new LyricsViewModel
+                {
+                    Title = string.Empty,
+                    StaticLyrics = string.Empty
+                };
+                this.trackInfo = null;
+                return;
+            }
+
+            this.trackInfo = trackInfo;
+
+            // The track didn't change: leave the previous lyrics.
+            if (this.trackInfo.Equals(this.previousTrackInfo)) return;
+
+            // The track changed: we need to show new lyrics.
+            try
             {
                 var fmd = new FileMetadata(trackInfo.Path);
 
-                this.Title = trackInfo.TrackTitle;
-                this.StaticLyrics = string.IsNullOrWhiteSpace(fmd.Lyrics.Value) ? "Not available" : fmd.Lyrics.Value;
-            });
+                this.LyricsViewModel = new LyricsViewModel
+                {
+                    Title = trackInfo.TrackTitle,
+                    StaticLyrics = string.IsNullOrWhiteSpace(fmd.Lyrics.Value) ? ResourceUtils.GetStringResource("Language_No_Lyrics") : fmd.Lyrics.Value
+                };
+            }
+            catch (Exception ex)
+            {
+                LogClient.Instance.Logger.Error("Could not show lyrics for Track {0}. Exception: {1}", trackInfo.Path, ex.Message);
+                this.LyricsViewModel = new LyricsViewModel
+                {
+                    Title = string.Empty,
+                    StaticLyrics = string.Empty
+                };
+            }
         }
         #endregion
     }
