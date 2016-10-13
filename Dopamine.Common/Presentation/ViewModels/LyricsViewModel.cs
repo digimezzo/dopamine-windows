3543e35034dfee1fc5db01e48a4c7c2d851d3f79
Raphaël <raphael.godart@outlook.com>
2016-10-13 20:12:38 +0200

    Building the lyrics screen

---
+++ Dopamine.Common/Presentation/ViewModels/LyricsViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Prism.Mvvm;
+
+namespace Dopamine.Common.Presentation.ViewModels
+{
+    public class LyricsViewModel : BindableBase
+    {
+        #region Properties
+        private string title;
+        private string staticLyrics;
+        #endregion
+
+        #region Properties
+        public string Title
+        {
+            get { return this.title; }
+            set { SetProperty<string>(ref this.title, value); }
+        }
+
+        public string StaticLyrics
+        {
+            get { return this.staticLyrics; }
+            set { SetProperty<string>(ref this.staticLyrics, value); }
+        }
+        #endregion
+    }
+}
