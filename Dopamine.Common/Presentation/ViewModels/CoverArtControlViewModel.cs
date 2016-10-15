5c75db7afa1ce324551341618ab3490e327d6050
Raphaël <raphael.godart@outlook.com>
2016-10-15 23:43:14 +0200

    Fixes #169: Do a better job at showing album art

--- Dopamine.Common/Presentation/ViewModels/CoverArtControlViewModel.cs
+++ Dopamine.Common/Presentation/ViewModels/CoverArtControlViewModel.cs
@@ -2,17 +2,15 @@
 using Dopamine.Common.Services.Cache;
 using Dopamine.Common.Services.Playback;
 using Dopamine.Core.Database;
-using Dopamine.Core.Database.Entities;
 using Dopamine.Core.IO;
 using Dopamine.Core.Logging;
-using Dopamine.Core.Utils;
+using Dopamine.Core.Metadata;
 using Prism.Mvvm;
 using System;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
 
 namespace Dopamine.Common.Presentation.ViewModels
 {
@@ -23,8 +21,7 @@ namespace Dopamine.Common.Presentation.ViewModels
         protected IPlaybackService playbackService;
         private ICacheService cacheService;
         private SlideDirection slideDirection;
-        private Album previousAlbum;
-        private Album album;
+        private int previousArtworkSize;
         #endregion
 
         #region Properties
@@ -79,28 +76,49 @@ namespace Dopamine.Common.Presentation.ViewModels
         #region Virtual
         protected async virtual void ShowCoverArtAsync(TrackInfo trackInfo)
         {
-            this.previousAlbum = this.album;
-
             // No track selected: clear cover art.
             if (trackInfo == null)
             {
                 this.CoverArtViewModel = new CoverArtViewModel { CoverArt = null };
-                this.album = null;
                 return;
             }
 
-            this.album = new Album
+            // 1. Try to find File artwork
+            byte[] fileArtWorkData = null;
+
+            await Task.Run(() =>
             {
-                AlbumArtist = this.playbackService.PlayingTrack.AlbumArtist,
-                AlbumTitle = this.playbackService.PlayingTrack.AlbumTitle,
-                Year = this.playbackService.PlayingTrack.AlbumYear,
-                ArtworkID = this.playbackService.PlayingTrack.AlbumArtworkID,
-            };
+                var fmd = new FileMetadata(trackInfo.Path);
+                fileArtWorkData = fmd.ArtworkData.Value;
+            });
 
-            // The album didn't change: leave the previous covert art.
-            if (this.album.Equals(this.previousAlbum)) return;
+            if(fileArtWorkData != null)
+            {
+                // If the new artwork has the same byte size as the previous artwork, it is most likely the same.
+                if (fileArtWorkData.Length == previousArtworkSize) return;
 
-            // The album changed: we need to show new cover art.
+                previousArtworkSize = fileArtWorkData.Length;
+
+                try
+                {
+                    Application.Current.Dispatcher.Invoke(() =>
+                    {
+                        var proxyImage = new Image();
+                        proxyImage.Stretch = Stretch.Fill;
+                        proxyImage.Source = ImageOperations.ByteToBitmapImage(fileArtWorkData, 0, 0);
+                        this.CoverArtViewModel = new CoverArtViewModel { CoverArt = proxyImage };
+                    });
+                }
+                catch (Exception ex)
+                {
+                    LogClient.Instance.Logger.Error("Could not show file artwork for Track {0}. Exception: {1}", trackInfo.Path, ex.Message);
+                    this.CoverArtViewModel = new CoverArtViewModel { CoverArt = null };
+                }
+
+                return;
+            }
+
+            // 2. If no file artwork was found, try to find album artwork.
             string artworkPath = string.Empty;
 
             await Task.Run(() =>
@@ -116,17 +134,32 @@ namespace Dopamine.Common.Presentation.ViewModels
 
             try
             {
-                Application.Current.Dispatcher.Invoke(() =>
+                byte[] albumArtWorkData = null;
+
+                await Task.Run(() =>
                 {
-                    var proxyImage = new Image();
-                    proxyImage.Stretch = Stretch.Fill;
-                    proxyImage.Source = ImageOperations.PathToBitmapImage(artworkPath, 0, 0);
-                    this.CoverArtViewModel = new CoverArtViewModel { CoverArt = proxyImage };
+                    albumArtWorkData = ImageOperations.Image2ByteArray(artworkPath);
                 });
+                
+                if (albumArtWorkData != null)
+                {
+                    // If the new artwork has the same byte size as the previous artwork, it is most likely the same.
+                    if (albumArtWorkData.Length == previousArtworkSize) return;
+
+                    previousArtworkSize = albumArtWorkData.Length;
+
+                    Application.Current.Dispatcher.Invoke(() =>
+                    {
+                        var proxyImage = new Image();
+                        proxyImage.Stretch = Stretch.Fill;
+                        proxyImage.Source = ImageOperations.ByteToBitmapImage(albumArtWorkData, 0, 0);
+                        this.CoverArtViewModel = new CoverArtViewModel { CoverArt = proxyImage };
+                    });
+                }
             }
             catch (Exception ex)
             {
-                LogClient.Instance.Logger.Error("Could not show cover art for Track {0}. Exception: {1}", trackInfo.Path, ex.Message);
+                LogClient.Instance.Logger.Error("Could not show album artwork for Track {0}. Exception: {1}", trackInfo.Path, ex.Message);
                 this.CoverArtViewModel = new CoverArtViewModel { CoverArt = null };
             }
         }
