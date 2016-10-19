029511faf66ec225f8edd5aec7dabf9cff0e59ec
Digimezzo <raphael.godart@outlook.com>
2016-10-19 15:14:30 +0200

    Fixed a threading issue

--- Dopamine.Common/Presentation/ViewModels/LyricsControlViewModel.cs
+++ Dopamine.Common/Presentation/ViewModels/LyricsControlViewModel.cs
@@ -25,6 +25,7 @@ namespace Dopamine.Common.Presentation.ViewModels
         private Timer highlightTimer;
         private int highlightTimerIntervalMilliseconds = 100;
         private EventAggregator eventAggregator;
+        private Object lockObject = new Object();
         #endregion
 
         #region Properties
@@ -85,37 +86,42 @@ namespace Dopamine.Common.Presentation.ViewModels
 
             this.previousTrackInfo = this.trackInfo;
 
-            // No track selected: clear lyrics.
-            if (trackInfo == null)
-            {
-                this.LyricsViewModel = new LyricsViewModel();
-                this.trackInfo = null;
-                return;
-            }
+            await Task.Run(() => {
+                lock (lockObject)
+                {
+                    // No track selected: clear lyrics.
+                    if (trackInfo == null)
+                    {
+                        this.LyricsViewModel = new LyricsViewModel();
+                        this.trackInfo = null;
+                        return;
+                    }
 
-            this.trackInfo = trackInfo;
+                    this.trackInfo = trackInfo;
 
-            // The track didn't change: leave the previous lyrics.
-            if (this.trackInfo.Equals(this.previousTrackInfo))
-            {
-                this.highlightTimer.Start();
-                return;
-            }
+                    // The track didn't change: leave the previous lyrics.
+                    if (this.trackInfo.Equals(this.previousTrackInfo))
+                    {
+                        this.highlightTimer.Start();
+                        return;
+                    }
 
-            // The track changed: we need to show new lyrics.
-            try
-            {
-                var fmd = new FileMetadata(trackInfo.Path);
+                    // The track changed: we need to show new lyrics.
+                    try
+                    {
+                        var fmd = new FileMetadata(trackInfo.Path);
 
-                this.LyricsViewModel = new LyricsViewModel();
-                await this.LyricsViewModel.SetLyricsAsync(string.IsNullOrWhiteSpace(fmd.Lyrics.Value) ? ResourceUtils.GetStringResource("Language_No_Lyrics") : fmd.Lyrics.Value);
-                this.highlightTimer.Start();
-            }
-            catch (Exception ex)
-            {
-                LogClient.Instance.Logger.Error("Could not show lyrics for Track {0}. Exception: {1}", trackInfo.Path, ex.Message);
-                this.LyricsViewModel = new LyricsViewModel();
-            }
+                        this.LyricsViewModel = new LyricsViewModel();
+                        this.LyricsViewModel.SetLyrics(string.IsNullOrWhiteSpace(fmd.Lyrics.Value) ? ResourceUtils.GetStringResource("Language_No_Lyrics") : fmd.Lyrics.Value);
+                        this.highlightTimer.Start();
+                    }
+                    catch (Exception ex)
+                    {
+                        LogClient.Instance.Logger.Error("Could not show lyrics for Track {0}. Exception: {1}", trackInfo.Path, ex.Message);
+                        this.LyricsViewModel = new LyricsViewModel();
+                    }
+                }
+            });
         }
 
         private async Task HighlightLyricsLineAsync()
@@ -124,29 +130,32 @@ namespace Dopamine.Common.Presentation.ViewModels
 
             await Task.Run(() =>
             {
-                for (int i = 0; i < this.LyricsViewModel.LyricsLines.Count; i++)
+                lock (lockObject)
                 {
-                    double progressTime = this.playbackService.GetCurrentTime.TotalMilliseconds;
-
-                    double lyricsLineTime = this.LyricsViewModel.LyricsLines[i].Time.TotalMilliseconds;
-                    double nextLyricsLineTime = 0;
-
-                    int j = 1;
-
-                    while (i + j < this.LyricsViewModel.LyricsLines.Count && nextLyricsLineTime <= lyricsLineTime)
+                    for (int i = 0; i < this.LyricsViewModel.LyricsLines.Count; i++)
                     {
-                        nextLyricsLineTime = this.LyricsViewModel.LyricsLines[i + j].Time.TotalMilliseconds;
-                        j++;
-                    }
+                        double progressTime = this.playbackService.GetCurrentTime.TotalMilliseconds;
 
-                    if (progressTime >= lyricsLineTime & (nextLyricsLineTime >= progressTime | nextLyricsLineTime == 0))
-                    {
-                        this.LyricsViewModel.LyricsLines[i].IsHighlighted = true;
-                        this.eventAggregator.GetEvent<ScrollToHighlightedLyricsLine>().Publish(null);
-                    }
-                    else
-                    {
-                        this.LyricsViewModel.LyricsLines[i].IsHighlighted = false;
+                        double lyricsLineTime = this.LyricsViewModel.LyricsLines[i].Time.TotalMilliseconds;
+                        double nextLyricsLineTime = 0;
+
+                        int j = 1;
+
+                        while (i + j < this.LyricsViewModel.LyricsLines.Count && nextLyricsLineTime <= lyricsLineTime)
+                        {
+                            nextLyricsLineTime = this.LyricsViewModel.LyricsLines[i + j].Time.TotalMilliseconds;
+                            j++;
+                        }
+
+                        if (progressTime >= lyricsLineTime & (nextLyricsLineTime >= progressTime | nextLyricsLineTime == 0))
+                        {
+                            this.LyricsViewModel.LyricsLines[i].IsHighlighted = true;
+                            this.eventAggregator.GetEvent<ScrollToHighlightedLyricsLine>().Publish(null);
+                        }
+                        else
+                        {
+                            this.LyricsViewModel.LyricsLines[i].IsHighlighted = false;
+                        }
                     }
                 }
             });
