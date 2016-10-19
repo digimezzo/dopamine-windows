963e317b24d631df0ab9c4cb4ee409ffc304f326
Digimezzo <raphael.godart@outlook.com>
2016-10-19 12:17:09 +0200

    Now playing doesn't jump to playlist anymore when playback is stopped

--- Dopamine.ControlsModule/ViewModels/NowPlayingPlaybackControlsViewModel.cs
+++ Dopamine.ControlsModule/ViewModels/NowPlayingPlaybackControlsViewModel.cs
@@ -1,32 +1,16 @@
-﻿using Dopamine.Common.Presentation.ViewModels;
-using Dopamine.Common.Services.Playback;
-using Dopamine.Core.Utils;
-using Prism.Mvvm;
+﻿using Dopamine.Common.Services.Playback;
 
 namespace Dopamine.ControlsModule.ViewModels
 {
     public class NowPlayingPlaybackControlsViewModel : PlaybackControlsViewModelBase
     {
-        #region ReadOnly Properties
-        public bool HasPlaybackInfo
-        {
-            get { return !this.playbackService.IsStopped; }
-        }
-        #endregion
-
         #region Construction
         public NowPlayingPlaybackControlsViewModel(IPlaybackService playbackService) : base(playbackService)
         {
             this.playbackService.PlaybackStopped += (_, __) =>
             {
                 this.Reset();
-                OnPropertyChanged(() => this.HasPlaybackInfo);
             };
-
-            this.playbackService.PlaybackFailed += (_, __) => OnPropertyChanged(() => this.HasPlaybackInfo);
-            this.playbackService.PlaybackPaused += (_, __) => OnPropertyChanged(() => this.HasPlaybackInfo);
-            this.playbackService.PlaybackResumed += (_, __) => OnPropertyChanged(() => this.HasPlaybackInfo);
-            this.playbackService.PlaybackSuccess += (_) => OnPropertyChanged(() => this.HasPlaybackInfo);
         }
         #endregion
     }
