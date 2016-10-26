36d4ca452f9addab990104292076a95d10f01331
Raphaël <raphael.godart@outlook.com>
2016-10-26 20:40:10 +0200

    Fixed a nullreferenceexception

--- Dopamine/SharedAssemblyInfo.cs
+++ Dopamine/SharedAssemblyInfo.cs
@@ -2,11 +2,11 @@
 
 [assembly: AssemblyCompany("Digimezzo")]
 [assembly: AssemblyCopyright("Copyright Digimezzo © 2014 - 2016")]
-[assembly: AssemblyVersion("1.2.751.0")]
-[assembly: AssemblyFileVersion("1.2.751.0")]
+[assembly: AssemblyVersion("1.2.752.0")]
+[assembly: AssemblyFileVersion("1.2.752.0")]
 
 #if DEBUG
-[assembly: AssemblyInformationalVersion("1.2.751.0 Preview")]
+[assembly: AssemblyInformationalVersion("1.2.752.0 Preview")]
 #else
-[assembly: AssemblyInformationalVersion("1.2.751.0")]
+[assembly: AssemblyInformationalVersion("1.2.752.0")]
 #endif
