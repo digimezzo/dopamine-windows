296ee3c0c263e7318ff3582f6b132da0be36030a
Raphaël <raphael.godart@outlook.com>
2016-10-31 20:52:32 +0100

    Improved the rating button

---
+++ Dopamine.Common/Controls/LoveButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Dopamine.Common.Controls
+{
+    public class LoveButton : Control
+    {
+        #region Construction
+        static LoveButton()
+        {
+            //This OverrideMetadata call tells the system that this element wants to provide a style that is different than its base class.
+            //This style is defined in themes\generic.xaml
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(LoveButton), new FrameworkPropertyMetadata(typeof(LoveButton)));
+        }
+        #endregion
+    }
+}
