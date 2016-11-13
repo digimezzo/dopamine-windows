549fbccf279ccc5705f72428a5a0b0b0e08f8fd5
Raphaël <raphael.godart@outlook.com>
2016-11-13 19:18:37 +0100

    TrackInfo now inherits from Track

--- Dopamine.Core/Database/TrackInfo.cs
+++ Dopamine.Core/Database/TrackInfo.cs
@@ -3,39 +3,8 @@ using System.Collections.Generic;
 
 namespace Dopamine.Core.Database
 {
-    public class TrackInfo
+    public class TrackInfo : Track
     {
-        #region Track
-        public long TrackID { get; set; }
-        public long ArtistID { get; set; }
-        public long GenreID { get; set; }
-        public long AlbumID { get; set; }
-        public long FolderID { get; set; }
-        public string Path { get; set; }
-        public string SafePath { get; set; }
-        public string FileName { get; set; }
-        public string MimeType { get; set; }
-        public long? FileSize { get; set; }
-        public long? BitRate { get; set; }
-        public long? SampleRate { get; set; }
-        public string TrackTitle { get; set; }
-        public long? TrackNumber { get; set; }
-        public long? TrackCount { get; set; }
-        public long? DiscNumber { get; set; }
-        public long? DiscCount { get; set; }
-        public long? Duration { get; set; }
-        public long? Year { get; set; }
-        public long? Rating { get; set; }
-        public long? Love { get; set; }
-        public long? PlayCount { get; set; }
-        public long? SkipCount { get; set; }
-        public long DateAdded { get; set; }
-        public long? DateLastPlayed { get; set; }
-        public long DateLastSynced { get; set; }
-        public long DateFileModified { get; set; }
-        public string MetaDataHash { get; set; }
-        #endregion
-
         #region Artist
         public string ArtistName { get; set; }
         #endregion
@@ -59,23 +28,6 @@ namespace Dopamine.Core.Database
             l.Add(this);
 
             return l;
-        }
-        #endregion
-
-        #region Overrides
-        public override int GetHashCode()
-        {
-            return this.SafePath.GetHashCode();
-        }
-
-        public override bool Equals(object obj)
-        {
-            if (obj == null || !GetType().Equals(obj.GetType()))
-            {
-                return false;
-            }
-
-            return this.SafePath.Equals(((TrackInfo)obj).SafePath);
         }
         #endregion
     }
