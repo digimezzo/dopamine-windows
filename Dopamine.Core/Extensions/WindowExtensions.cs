cf6474e717fcd170d07d300be7b697e0bb3405df
Raphaël <raphael.godart@outlook.com>
2016-11-22 22:45:30 +0100

    Slight modification to the code to set window geometry

--- Dopamine.Core/Extensions/WindowExtensions.cs
+++ Dopamine.Core/Extensions/WindowExtensions.cs
@@ -6,15 +6,15 @@ namespace Dopamine.Core.Extensions
 {
     public static class WindowExtensions
     {
-        public static void SetGeometry(this Window win, double top, double left, double width, double height)
+        public static void SetGeometry(this Window win, double top, double left, double width, double height, double topFallback = 50, double leftFallback = 50)
         {
             if (left <= (SystemParameters.VirtualScreenLeft - width) ||
                 top <= (SystemParameters.VirtualScreenTop - height) ||
                 (SystemParameters.VirtualScreenLeft + SystemParameters.VirtualScreenWidth) <= left ||
                 (SystemParameters.VirtualScreenTop + SystemParameters.VirtualScreenHeight) <= top)
             {
-                top = Convert.ToInt32(Constants.DefaultShellTop);
-                left = Convert.ToInt32(Constants.DefaultShellLeft);
+                top = Convert.ToInt32(topFallback);
+                left = Convert.ToInt32(leftFallback);
             }
 
             win.Top = top;
