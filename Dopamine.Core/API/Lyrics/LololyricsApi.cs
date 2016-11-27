974cb4bd9a8a614742dd713a94ff4c9be99b200f
Raphaël <raphael.godart@outlook.com>
2016-11-27 10:57:07 +0100

    Improved online lyrics search

---
+++ Dopamine.Core/API/Lyrics/LololyricsApi.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Dopamine.Core.Api.Lyrics
+{
+    public class LololyricsApi : ILyricsApi
+    {
+        #region Variables
+        private const string apiRootFormat = "http://api.lololyrics.com/0.5/getLyric?artist={0}&track={1}";
+        #endregion
+
+        #region Private
+        public async Task<string> ParseResultAsync(string result)
+        {
+            string lyrics = string.Empty;
+
+            await Task.Run(() =>
+            {
+                if (!string.IsNullOrEmpty(result))
+                {
+                    // http://api.lololyrics.com/
+                    var resultXml = XDocument.Parse(result);
+
+                    // Status
+                    string status = (from t in resultXml.Element("result").Elements("status")
+                                     select t.Value).FirstOrDefault();
+
+                    if (status != null && status.ToLower() == "ok")
+                    {
+                        lyrics = (from t in resultXml.Element("result").Elements("response")
+                                  select t.Value).FirstOrDefault();
+                    }
+                }
+            });
+
+            return lyrics;
+        }
+        #endregion
+
+        #region ILyricsApi
+        /// <summary>
+        /// Searches for lyrics for the given artist and title
+        /// </summary>
+        /// <param name="artist"></param>
+        /// <param name="title"></param>
+        /// <returns></returns>
+        public async Task<string> GetLyricsAsync(string artist, string title)
+        {
+            Uri uri = new Uri(string.Format(apiRootFormat, artist, title));
+
+            string result = string.Empty;
+
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.ExpectContinue = false;
+                var response = await client.GetAsync(uri);
+                result = await response.Content.ReadAsStringAsync();
+            }
+
+            string lyrics = await ParseResultAsync(result);
+
+            return lyrics;
+        }
+        #endregion
+    }
+}
