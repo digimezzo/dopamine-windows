e47646e80f5c2f19dfff0e88d90815fc991793bd
Raphaël <raphael.godart@outlook.com>
2016-11-30 23:13:45 +0100

    Small playback changes

--- Dopamine.Common/Services/Playback/PlaybackService.cs
+++ Dopamine.Common/Services/Playback/PlaybackService.cs
@@ -1211,11 +1211,11 @@ namespace Dopamine.Common.Services.Playback
 
         private void PlaybackInterruptedHandler(Object sender, PlaybackInterruptedEventArgs e)
         {
-            LogClient.Instance.Logger.Error("Playback of track '{0}' was interrupted. Trying to play the next track anyway. Exception: {1}", this.playingTrack.Path, e.Message);
+            LogClient.Instance.Logger.Error("Playback of track '{0}' was interrupted. Trying to play the same track again. Exception: {1}", this.playingTrack.Path, e.Message);
 
-            // Try to play the next Track from the list automatically.
+            // Try to play the same Track again.
             // Use our context to trigger the work, because this event is fired on the Player's Playback thread.
-            this.context.Post(new SendOrPostCallback(async (state) => await this.TryPlayNextAsync()), null);
+            this.context.Post(new SendOrPostCallback(async (state) => await this.TryPlayAsync(this.playingTrack)), null);
         }
 
         private void PlaybackFinishedHandler(Object sender, EventArgs e)
