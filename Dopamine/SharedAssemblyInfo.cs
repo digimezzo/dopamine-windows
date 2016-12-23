e6a0457f72efa094bf2d90bb229ef3f68d20be90
Raphaël <raphael.godart@outlook.com>
2016-12-23 19:45:28 +0100

    Updated the change log in preparation of 1.2 release

--- Dopamine/SharedAssemblyInfo.cs
+++ Dopamine/SharedAssemblyInfo.cs
@@ -2,11 +2,11 @@
 
 [assembly: AssemblyCompany("Digimezzo")]
 [assembly: AssemblyCopyright("Copyright Digimezzo © 2014 - 2016")]
-[assembly: AssemblyVersion("1.2.841.0")]
-[assembly: AssemblyFileVersion("1.2.841.0")]
+[assembly: AssemblyVersion("1.2.842.0")]
+[assembly: AssemblyFileVersion("1.2.842.0")]
 
 #if DEBUG
-[assembly: AssemblyInformationalVersion("1.2.841.0 Preview")]
+[assembly: AssemblyInformationalVersion("1.2.842.0 Preview")]
 #else
-[assembly: AssemblyInformationalVersion("1.2.841.0")]
+[assembly: AssemblyInformationalVersion("1.2.842.0")]
 #endif
