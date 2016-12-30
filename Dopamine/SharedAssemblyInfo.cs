b770ff67878ef7cfdbba638d6502e27a184efa5f
Raphaël <raphael.godart@outlook.com>
2016-12-30 14:54:29 +0100

    Better fix for pinned icon

--- Dopamine/SharedAssemblyInfo.cs
+++ Dopamine/SharedAssemblyInfo.cs
@@ -2,11 +2,11 @@
 
 [assembly: AssemblyCompany("Digimezzo")]
 [assembly: AssemblyCopyright("Copyright Digimezzo © 2014 - 2017")]
-[assembly: AssemblyVersion("1.2.851.2")]
-[assembly: AssemblyFileVersion("1.2.851.2")]
+[assembly: AssemblyVersion("1.2.852.2")]
+[assembly: AssemblyFileVersion("1.2.852.2")]
 
 #if DEBUG
-[assembly: AssemblyInformationalVersion("1.2.851.2 Preview")]
+[assembly: AssemblyInformationalVersion("1.2.852.2 Preview")]
 #else
-[assembly: AssemblyInformationalVersion("1.2.851.2")]
+[assembly: AssemblyInformationalVersion("1.2.852.2")]
 #endif
