d38f191dc4447a68dda9eda36d09801de6751119
Raphaël <raphael.godart@outlook.com>
2016-12-30 09:25:16 +0100

    Updated wxs

--- Dopamine/SharedAssemblyInfo.cs
+++ Dopamine/SharedAssemblyInfo.cs
@@ -1,12 +1,12 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyCompany("Digimezzo")]
-[assembly: AssemblyCopyright("Copyright Digimezzo © 2014 - 2016")]
-[assembly: AssemblyVersion("1.2.850.2")]
-[assembly: AssemblyFileVersion("1.2.850.2")]
+[assembly: AssemblyCopyright("Copyright Digimezzo © 2014 - 2017")]
+[assembly: AssemblyVersion("1.2.851.2")]
+[assembly: AssemblyFileVersion("1.2.851.2")]
 
 #if DEBUG
-[assembly: AssemblyInformationalVersion("1.2.850.2 Preview")]
+[assembly: AssemblyInformationalVersion("1.2.851.2 Preview")]
 #else
-[assembly: AssemblyInformationalVersion("1.2.850.2")]
+[assembly: AssemblyInformationalVersion("1.2.851.2")]
 #endif
