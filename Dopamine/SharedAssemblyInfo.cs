a4075c9fbc762a2cc1913422736fae9ff7f51d2d
Raphaël <raphael.godart@outlook.com>
2017-01-02 20:58:31 +0100

    [Fixed] Artists are not sorted alphabetically

--- Dopamine/SharedAssemblyInfo.cs
+++ Dopamine/SharedAssemblyInfo.cs
@@ -2,11 +2,11 @@
 
 [assembly: AssemblyCompany("Digimezzo")]
 [assembly: AssemblyCopyright("Copyright Digimezzo © 2014 - 2017")]
-[assembly: AssemblyVersion("1.2.860.3")]
-[assembly: AssemblyFileVersion("1.2.860.3")]
+[assembly: AssemblyVersion("1.2.861.4")]
+[assembly: AssemblyFileVersion("1.2.861.4")]
 
 #if DEBUG
-[assembly: AssemblyInformationalVersion("1.2.860.3 Preview")]
+[assembly: AssemblyInformationalVersion("1.2.861.4 Preview")]
 #else
-[assembly: AssemblyInformationalVersion("1.2.860.3")]
+[assembly: AssemblyInformationalVersion("1.2.861.4")]
 #endif
