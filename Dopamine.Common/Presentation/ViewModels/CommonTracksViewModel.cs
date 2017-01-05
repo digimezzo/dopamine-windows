b321f6764ee3597883c6ea63b4a7747d52d30518
PeoLeser <peoleser@gmail.com>
2017-01-05 22:10:48 +0900

    Enhance #241(remove useless and update languages)

--- Dopamine.Common/Presentation/ViewModels/CommonTracksViewModel.cs
+++ Dopamine.Common/Presentation/ViewModels/CommonTracksViewModel.cs
@@ -27,7 +27,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
