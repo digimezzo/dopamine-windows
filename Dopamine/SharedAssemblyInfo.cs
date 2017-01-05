e4c7450d092fbd746485d4b2ee838d7efe53647b
Raphaël <raphael.godart@outlook.com>
2017-01-05 22:51:59 +0100

    Fixes #257: Update notification not showing revision number

--- Dopamine/SharedAssemblyInfo.cs
+++ Dopamine/SharedAssemblyInfo.cs
@@ -2,11 +2,11 @@
 
 [assembly: AssemblyCompany("Digimezzo")]
 [assembly: AssemblyCopyright("Copyright Digimezzo © 2014 - 2017")]
-[assembly: AssemblyVersion("1.3.0.863")]
-[assembly: AssemblyFileVersion("1.3.0.863")]
+[assembly: AssemblyVersion("1.2.0.1")]
+[assembly: AssemblyFileVersion("1.2.0.1")]
 
 #if DEBUG
-[assembly: AssemblyInformationalVersion("1.3.0.863 Preview")]
+[assembly: AssemblyInformationalVersion("1.2.0.1 Preview")]
 #else
-[assembly: AssemblyInformationalVersion("1.3.0.863")]
+[assembly: AssemblyInformationalVersion("1.2.0.1")]
 #endif
