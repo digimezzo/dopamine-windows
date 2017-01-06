96c6f08844e5af9debd78ea8e863792114927a37
Raphaël <raphael.godart@outlook.com>
2017-01-06 08:53:21 +0100

    Corrected assembly version. I forgot to restore it after performing upgrade tests.

--- Dopamine/SharedAssemblyInfo.cs
+++ Dopamine/SharedAssemblyInfo.cs
@@ -2,11 +2,11 @@
 
 [assembly: AssemblyCompany("Digimezzo")]
 [assembly: AssemblyCopyright("Copyright Digimezzo © 2014 - 2017")]
-[assembly: AssemblyVersion("1.2.0.1")]
-[assembly: AssemblyFileVersion("1.2.0.1")]
+[assembly: AssemblyVersion("1.3.0.865")]
+[assembly: AssemblyFileVersion("1.3.0.865")]
 
 #if DEBUG
-[assembly: AssemblyInformationalVersion("1.2.0.1 Preview")]
+[assembly: AssemblyInformationalVersion("1.3.0.865 Preview")]
 #else
-[assembly: AssemblyInformationalVersion("1.2.0.1")]
+[assembly: AssemblyInformationalVersion("1.3.0.865")]
 #endif
