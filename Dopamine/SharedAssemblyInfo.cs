0e931b93ff2169dda3325bcd505520937dfa8c5e
Raphaël <raphael.godart@outlook.com>
2017-01-08 09:26:38 +0100

    Tweaked scalingTextBlock, bumped assembly version.

--- Dopamine/SharedAssemblyInfo.cs
+++ Dopamine/SharedAssemblyInfo.cs
@@ -2,11 +2,11 @@
 
 [assembly: AssemblyCompany("Digimezzo")]
 [assembly: AssemblyCopyright("Copyright Digimezzo © 2014 - 2017")]
-[assembly: AssemblyVersion("1.3.0.868")]
-[assembly: AssemblyFileVersion("1.3.0.868")]
+[assembly: AssemblyVersion("1.3.0.870")]
+[assembly: AssemblyFileVersion("1.3.0.870")]
 
 #if DEBUG
-[assembly: AssemblyInformationalVersion("1.3.0.868 Preview")]
+[assembly: AssemblyInformationalVersion("1.3.0.870 Preview")]
 #else
-[assembly: AssemblyInformationalVersion("1.3.0.868")]
+[assembly: AssemblyInformationalVersion("1.3.0.870")]
 #endif
