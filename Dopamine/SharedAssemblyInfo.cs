5ee01b3d683d096167656fd718695f39ea3789ed
Raphaël <raphael.godart@outlook.com>
2017-01-17 23:14:13 +0100

    Fixes #296: "Cloud" screen needs a better name

--- Dopamine/SharedAssemblyInfo.cs
+++ Dopamine/SharedAssemblyInfo.cs
@@ -2,11 +2,11 @@
 
 [assembly: AssemblyCompany("Digimezzo")]
 [assembly: AssemblyCopyright("Copyright Digimezzo © 2014 - 2017")]
-[assembly: AssemblyVersion("1.3.0.877")]
-[assembly: AssemblyFileVersion("1.3.0.877")]
+[assembly: AssemblyVersion("1.3.0.878")]
+[assembly: AssemblyFileVersion("1.3.0.878")]
 
 #if DEBUG
-[assembly: AssemblyInformationalVersion("1.3.0.877 Preview")]
+[assembly: AssemblyInformationalVersion("1.3.0.878 Preview")]
 #else
-[assembly: AssemblyInformationalVersion("1.3.0.877")]
+[assembly: AssemblyInformationalVersion("1.3.0.878")]
 #endif
