38bca67cc8c687f9a69c90627f756dc8cd63da1c
Raphaël <raphael.godart@outlook.com>
2017-02-02 11:56:16 +0100

    Removed Dopamine.Core files

--- Dopamine.Core/Database/Repositories/Interfaces/IPlaylistEntryRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace Dopamine.Core.Database.Repositories.Interfaces
-{
-    public interface IPlaylistEntryRepository
-    {
-        Task DeleteOrphanedPlaylistEntriesAsync();
-    }
-}
