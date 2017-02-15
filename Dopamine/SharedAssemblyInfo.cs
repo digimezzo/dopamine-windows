648d3f2b29907fc6e6519b2af270954d67b61b57
raphael <raphael@DESKTOP-64I0SI3>
2017-02-15 10:39:07 +0100

    More tweaks to frequent screen. Removed broken workaround for pinned icons.

--- Dopamine/SharedAssemblyInfo.cs
+++ Dopamine/SharedAssemblyInfo.cs
@@ -2,11 +2,11 @@
 
 [assembly: AssemblyCompany("Digimezzo")]
 [assembly: AssemblyCopyright("Copyright Digimezzo © 2014 - 2017")]
-[assembly: AssemblyVersion("1.3.0.891")]
-[assembly: AssemblyFileVersion("1.3.0.891")]
+[assembly: AssemblyVersion("1.3.0.894")]
+[assembly: AssemblyFileVersion("1.3.0.894")]
 
 #if DEBUG
-[assembly: AssemblyInformationalVersion("1.3.0.891 Preview")]
+[assembly: AssemblyInformationalVersion("1.3.0.894 Preview")]
 #else
-[assembly: AssemblyInformationalVersion("1.3.0.891")]
+[assembly: AssemblyInformationalVersion("1.3.0.894")]
 #endif
