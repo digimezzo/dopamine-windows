8e6ab92a8ae2c9daf2dc30a7fa416fcf744542c8
Raphaël <raphael.godart@outlook.com>
2017-03-03 07:56:48 +0100

    Updated the Polish translation

--- Dopamine/SharedAssemblyInfo.cs
+++ Dopamine/SharedAssemblyInfo.cs
@@ -2,11 +2,11 @@
 
 [assembly: AssemblyCompany("Digimezzo")]
 [assembly: AssemblyCopyright("Copyright Digimezzo © 2014 - 2017")]
-[assembly: AssemblyVersion("1.3.0.901")]
-[assembly: AssemblyFileVersion("1.3.0.901")]
+[assembly: AssemblyVersion("1.3.0.902")]
+[assembly: AssemblyFileVersion("1.3.0.902")]
 
 #if DEBUG
-[assembly: AssemblyInformationalVersion("1.3.0.901 Preview")]
+[assembly: AssemblyInformationalVersion("1.3.0.902 Preview")]
 #else
-[assembly: AssemblyInformationalVersion("1.3.0.901")]
+[assembly: AssemblyInformationalVersion("1.3.0.902")]
 #endif
