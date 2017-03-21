b8ea41e8dfd34027dfe2cbbf425e866796faedf9
Raphaël <raphael.godart@outlook.com>
2017-03-21 21:37:09 +0100

    Refactored unit tests

---
+++ Dopamine.Tests/TestCategories.cs
@@ -0,0 +1,10 @@
+﻿namespace Dopamine.Tests
+{
+    public static class TestCategories
+    {
+        public const string AudioFormatsMediaFoundation = "Audio formats (Media Foundation)";
+        public const string LyricsApi = "Lyrics API";
+        public const string LastfmApi = "Last.fm API";
+        public const string PlaylistDecoder = "Playlist decoder";
+    }
+}
