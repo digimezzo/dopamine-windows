f6868dd1b7f2a84743fc476a9591b5f851efad39
Raphaël <raphael.godart@outlook.com>
2017-04-01 00:05:00 +0200

    Updated change log

--- Dopamine/SharedAssemblyInfo.cs
+++ Dopamine/SharedAssemblyInfo.cs
@@ -2,11 +2,11 @@
 
 [assembly: AssemblyCompany("Digimezzo")]
 [assembly: AssemblyCopyright("Copyright Digimezzo © 2014 - 2017")]
-[assembly: AssemblyVersion("1.3.0.909")]
-[assembly: AssemblyFileVersion("1.3.0.909")]
+[assembly: AssemblyVersion("1.3.0.910")]
+[assembly: AssemblyFileVersion("1.3.0.910")]
 
 #if DEBUG
-[assembly: AssemblyInformationalVersion("1.3.0.909 Preview")]
+[assembly: AssemblyInformationalVersion("1.3.0.910 Preview")]
 #else
-[assembly: AssemblyInformationalVersion("1.3.0.909")]
+[assembly: AssemblyInformationalVersion("1.3.0.910")]
 #endif
