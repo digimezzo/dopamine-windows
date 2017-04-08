f014a08d1d248bdb50dd6ffaeecba37c2c1db0d6
PeoLeser <peoleser@gmail.com>
2017-04-08 15:10:20 +0900

    Revert prev change

--- Dopamine/Views/Shell.xaml.cs
+++ Dopamine/Views/Shell.xaml.cs
@@ -241,7 +241,7 @@ namespace Dopamine.Views
 
             // Application
             // -----------
-            this.ShowMainWindowCommand = new DelegateCommand(() => TrayIconContextMenuAppName_Click(null, null));
+            this.ShowMainWindowCommand = new DelegateCommand(() => this.ActivateNow());
             Common.Prism.ApplicationCommands.ShowMainWindowCommand.RegisterCommand(this.ShowMainWindowCommand);
         }
 
