448669d1c525802c3769df418a9f3eaec21f1b29
Raphaël <raphael.godart@outlook.com>
2017-05-01 12:28:52 +0200

    Bumped assembly version

--- Dopamine/SharedAssemblyInfo.cs
+++ Dopamine/SharedAssemblyInfo.cs
@@ -2,11 +2,11 @@
 
 [assembly: AssemblyCompany("Digimezzo")]
 [assembly: AssemblyCopyright("Copyright Digimezzo © 2014 - 2017")]
-[assembly: AssemblyVersion("1.3.0.916")]
-[assembly: AssemblyFileVersion("1.3.0.916")]
+[assembly: AssemblyVersion("1.3.0.917")]
+[assembly: AssemblyFileVersion("1.3.0.917")]
 
 #if DEBUG
-[assembly: AssemblyInformationalVersion("1.3.0.916 Preview")]
+[assembly: AssemblyInformationalVersion("1.3.0.917 Preview")]
 #else
-[assembly: AssemblyInformationalVersion("1.3.0.916")]
+[assembly: AssemblyInformationalVersion("1.3.0.917")]
 #endif
