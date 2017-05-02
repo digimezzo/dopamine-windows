a0136582d34032788b231df1e9f227f4e59422b6
Raphaël <raphael.godart@outlook.com>
2017-05-02 20:34:44 +0200

    Fixed typing in the search box doesn't work anymore, updated Swedish translation.

--- Dopamine/SharedAssemblyInfo.cs
+++ Dopamine/SharedAssemblyInfo.cs
@@ -2,11 +2,11 @@
 
 [assembly: AssemblyCompany("Digimezzo")]
 [assembly: AssemblyCopyright("Copyright Digimezzo © 2014 - 2017")]
-[assembly: AssemblyVersion("1.3.0.917")]
-[assembly: AssemblyFileVersion("1.3.0.917")]
+[assembly: AssemblyVersion("1.3.0.918")]
+[assembly: AssemblyFileVersion("1.3.0.918")]
 
 #if DEBUG
-[assembly: AssemblyInformationalVersion("1.3.0.917 Preview")]
+[assembly: AssemblyInformationalVersion("1.3.0.918 Preview")]
 #else
-[assembly: AssemblyInformationalVersion("1.3.0.917")]
+[assembly: AssemblyInformationalVersion("1.3.0.918")]
 #endif
