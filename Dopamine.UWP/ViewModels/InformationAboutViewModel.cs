ad250b440f9993f7cb626b8de8e57cfb705bdf4a
Raphaël <raphael.godart@outlook.com>
2017-07-31 21:14:41 +0200

    Added UWP version number

--- Dopamine.UWP/ViewModels/InformationAboutViewModel.cs
+++ Dopamine.UWP/ViewModels/InformationAboutViewModel.cs
@@ -1,8 +1,15 @@
-﻿using GalaSoft.MvvmLight;
+﻿using Dopamine.UWP.Base;
+using GalaSoft.MvvmLight;
 
 namespace Dopamine.UWP.ViewModels
 {
     public class InformationAboutViewModel : ViewModelBase
     {
+        #region Properties
+        public string AssemblyVersion
+        {
+            get { return ProductInformation.AssemblyVersion; }
+        }
+        #endregion
     }
 }
