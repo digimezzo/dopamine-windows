589be5d965a90f655fd4d1c86c9dd05e22bddd03
Raphaël <raphael.godart@outlook.com>
2017-07-31 21:04:18 +0200

    Further building of UWP screens

---
+++ Dopamine.Core/Base/ProductInformation.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Dopamine.Core.Base
+{
+    public abstract class ProductInformation
+    {
+        public static string ApplicationDisplayName = "Dopamine";
+        public static string Copyright = "Copyright Digimezzo © 2014-" + DateTime.Now.Year;
+    }
+}
