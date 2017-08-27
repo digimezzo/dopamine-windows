92a20c1cba03c3c6171d1c2d1cf3c8f05b604aca
Raphaël <raphael.godart@outlook.com>
2017-08-27 20:16:51 +0200

    Added a project to create the Dopamine.Icons.dll, added Dopamine.Core.dll to the packaging, updated the change log.

--- Dopamine/SharedAssemblyInfo.cs
+++ Dopamine/SharedAssemblyInfo.cs
@@ -2,11 +2,11 @@
 
 [assembly: AssemblyCompany("Digimezzo")]
 [assembly: AssemblyCopyright("Copyright Digimezzo © 2014 - 2017")]
-[assembly: AssemblyVersion("1.3.1.928")]
-[assembly: AssemblyFileVersion("1.3.1.928")]
+[assembly: AssemblyVersion("1.3.2.930")]
+[assembly: AssemblyFileVersion("1.3.2.930")]
 
 #if DEBUG
-[assembly: AssemblyInformationalVersion("1.3.1.928 Preview")]
+[assembly: AssemblyInformationalVersion("1.3.1.930 Preview")]
 #else
-[assembly: AssemblyInformationalVersion("1.3.1.928")]
+[assembly: AssemblyInformationalVersion("1.3.2.930")]
 #endif
