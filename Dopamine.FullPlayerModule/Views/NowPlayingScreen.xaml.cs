2b3c2d157e6b09e53775ca07ddd215f46996a621
PeoLeser <peoleser@gmail.com>
2017-09-09 10:07:07 +0900

    double FadeOutDuration

--- Dopamine.FullPlayerModule/Views/NowPlayingScreen.xaml.cs
+++ Dopamine.FullPlayerModule/Views/NowPlayingScreen.xaml.cs
@@ -114,7 +114,7 @@ namespace Dopamine.FullPlayerModule.Views
             await Task.Delay(500);
             this.NowPlayingContentRegion.SlideDuration = Constants.SlideTimeoutSeconds * 1.5;
             this.NowPlayingContentRegion.FadeInDuration = Constants.FadeInTimeoutSeconds *1.25;
-            this.NowPlayingContentRegion.FadeOutDuration = Constants.FadeOutTimeoutSeconds *1.25;
+            this.NowPlayingContentRegion.FadeOutDuration = Constants.FadeOutTimeoutSeconds *2;
         }
         #endregion  
     }
