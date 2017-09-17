d5e5eb340611350ba7924c46f27bd2b6eade58a8
Raphaël <raphael.godart@outlook.com>
2017-09-17 07:12:30 +0200

    Revert "Removed the Icons project. It was broken. Replaces heart icon by a coffee icon in the jumplist"
    This reverts commit b104cd98399fca940d5ebb43068e2860ed2c2ccb.

--- Dopamine/SharedAssemblyInfo.cs
+++ Dopamine/SharedAssemblyInfo.cs
@@ -2,11 +2,11 @@
 
 [assembly: AssemblyCompany("Digimezzo")]
 [assembly: AssemblyCopyright("Copyright Digimezzo © 2014 - 2017")]
-[assembly: AssemblyVersion("1.4.0.936")]
-[assembly: AssemblyFileVersion("1.4.0.936")]
+[assembly: AssemblyVersion("1.4.0.935")]
+[assembly: AssemblyFileVersion("1.4.0.935")]
 
 #if DEBUG
-[assembly: AssemblyInformationalVersion("1.4.0.936 Preview")]
+[assembly: AssemblyInformationalVersion("1.4.0.935 Preview")]
 #else
-[assembly: AssemblyInformationalVersion("1.4.0.936")]
+[assembly: AssemblyInformationalVersion("1.4.0.935")]
 #endif
