5abb4ae67ee41f78ab369593c4b01723e398c146
Raphaël <raphael.godart@outlook.com>
2017-09-22 23:45:21 +0200

    Double clicking a file in explorer starts Dopamine as a Mini player

--- Dopamine/SharedAssemblyInfo.cs
+++ Dopamine/SharedAssemblyInfo.cs
@@ -2,11 +2,11 @@
 
 [assembly: AssemblyCompany("Digimezzo")]
 [assembly: AssemblyCopyright("Copyright Digimezzo © 2014 - 2017")]
-[assembly: AssemblyVersion("1.4.0.937")]
-[assembly: AssemblyFileVersion("1.4.0.937")]
+[assembly: AssemblyVersion("1.4.0.940")]
+[assembly: AssemblyFileVersion("1.4.0.940")]
 
 #if DEBUG
-[assembly: AssemblyInformationalVersion("1.4.0.937 Preview")]
+[assembly: AssemblyInformationalVersion("1.4.0.940 Preview")]
 #else
-[assembly: AssemblyInformationalVersion("1.4.0.937")]
+[assembly: AssemblyInformationalVersion("1.4.0.940")]
 #endif
