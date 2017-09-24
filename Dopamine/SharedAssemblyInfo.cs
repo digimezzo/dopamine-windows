e23b2af03676dd5f73ca2b7905c712cbf464b50e
Raphaël <raphael.godart@outlook.com>
2017-09-24 08:24:01 +0200

    [Fixed] Tracks on the playlists screen are not cleared when the last playlist is deleted

--- Dopamine/SharedAssemblyInfo.cs
+++ Dopamine/SharedAssemblyInfo.cs
@@ -2,11 +2,11 @@
 
 [assembly: AssemblyCompany("Digimezzo")]
 [assembly: AssemblyCopyright("Copyright Digimezzo © 2014 - 2017")]
-[assembly: AssemblyVersion("1.4.0.946")]
-[assembly: AssemblyFileVersion("1.4.0.946")]
+[assembly: AssemblyVersion("1.4.0.947")]
+[assembly: AssemblyFileVersion("1.4.0.947")]
 
 #if DEBUG
-[assembly: AssemblyInformationalVersion("1.4.0.946 Preview")]
+[assembly: AssemblyInformationalVersion("1.4.0.947 Preview")]
 #else
-[assembly: AssemblyInformationalVersion("1.4.0.946")]
+[assembly: AssemblyInformationalVersion("1.4.0.947")]
 #endif
