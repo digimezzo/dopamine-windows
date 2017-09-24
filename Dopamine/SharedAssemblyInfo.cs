1e2f027ff8cd455ce4fe5861af2ccc8804e6da41
Raphaël <raphael.godart@outlook.com>
2017-09-24 12:21:57 +0200

    Tweaked the list of similar artists

--- Dopamine/SharedAssemblyInfo.cs
+++ Dopamine/SharedAssemblyInfo.cs
@@ -2,11 +2,11 @@
 
 [assembly: AssemblyCompany("Digimezzo")]
 [assembly: AssemblyCopyright("Copyright Digimezzo © 2014 - 2017")]
-[assembly: AssemblyVersion("1.4.0.948")]
-[assembly: AssemblyFileVersion("1.4.0.948")]
+[assembly: AssemblyVersion("1.4.0.949")]
+[assembly: AssemblyFileVersion("1.4.0.949")]
 
 #if DEBUG
-[assembly: AssemblyInformationalVersion("1.4.0.948 Preview")]
+[assembly: AssemblyInformationalVersion("1.4.0.949 Preview")]
 #else
-[assembly: AssemblyInformationalVersion("1.4.0.948")]
+[assembly: AssemblyInformationalVersion("1.4.0.949")]
 #endif
