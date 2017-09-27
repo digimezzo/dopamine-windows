3fe6e0327e771f19fb35ae4ca6427d807c3a03a2
Raphaël <raphael.godart@outlook.com>
2017-09-27 13:47:28 +0200

    Added information about external control to the change log

--- Dopamine/SharedAssemblyInfo.cs
+++ Dopamine/SharedAssemblyInfo.cs
@@ -2,11 +2,11 @@
 
 [assembly: AssemblyCompany("Digimezzo")]
 [assembly: AssemblyCopyright("Copyright Digimezzo © 2014 - 2017")]
-[assembly: AssemblyVersion("1.4.0.950")]
-[assembly: AssemblyFileVersion("1.4.0.950")]
+[assembly: AssemblyVersion("1.4.0.951")]
+[assembly: AssemblyFileVersion("1.4.0.951")]
 
 #if DEBUG
-[assembly: AssemblyInformationalVersion("1.4.0.950 Preview")]
+[assembly: AssemblyInformationalVersion("1.4.0.951 Preview")]
 #else
-[assembly: AssemblyInformationalVersion("1.4.0.950")]
+[assembly: AssemblyInformationalVersion("1.4.0.951")]
 #endif
