0dad03f6fd34f9f9afb7e43d63839355a67c25d8
Raphaël <raphael.godart@outlook.com>
2017-09-27 13:40:37 +0200

    Finished track art toggle

--- Dopamine/SharedAssemblyInfo.cs
+++ Dopamine/SharedAssemblyInfo.cs
@@ -2,11 +2,11 @@
 
 [assembly: AssemblyCompany("Digimezzo")]
 [assembly: AssemblyCopyright("Copyright Digimezzo © 2014 - 2017")]
-[assembly: AssemblyVersion("1.4.0.949")]
-[assembly: AssemblyFileVersion("1.4.0.949")]
+[assembly: AssemblyVersion("1.4.0.950")]
+[assembly: AssemblyFileVersion("1.4.0.950")]
 
 #if DEBUG
-[assembly: AssemblyInformationalVersion("1.4.0.949 Preview")]
+[assembly: AssemblyInformationalVersion("1.4.0.950 Preview")]
 #else
-[assembly: AssemblyInformationalVersion("1.4.0.949")]
+[assembly: AssemblyInformationalVersion("1.4.0.950")]
 #endif
