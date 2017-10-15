b6881328adbf40a717fd3f3fe8b5d322e037402a
Raphaël <raphael.godart@outlook.com>
2017-10-15 22:59:10 +0200

    Added missing ViewModel

---
+++ Dopamine/ViewModels/NowPlaying/NowPlayingShowcaseViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Dopamine.Common.Presentation.ViewModels.Base;
+using Microsoft.Practices.Unity;
+
+namespace Dopamine.ViewModels.NowPlaying
+{
+    public class NowPlayingShowcaseViewModel : ContextMenuViewModelBase
+    {
+        public NowPlayingShowcaseViewModel(IUnityContainer container) : base(container)
+        {
+        }
+
+        protected override void SearchOnline(string id)
+        {
+            // No implementation required here
+        }
+    }
+}
