2bf5220826009f34a22da0548c590b6dea6daf22
Raphaël <raphael.godart@outlook.com>
2017-10-15 22:56:04 +0200

    Added Now Playing showcase screen

---
+++ Dopamine/Views/NowPlaying/NowPlayingShowcase.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows.Controls;
+using System.Windows.Media.Animation;
+
+namespace Dopamine.Views.NowPlaying
+{
+    public partial class NowPlayingShowcase : UserControl
+    {
+        public NowPlayingShowcase()
+        {
+            InitializeComponent();
+        }
+
+        private void ResizePlaybackInfo()
+        {
+            Storyboard resizeCoverArtStoryboard = null;
+
+            for (int index = 250; index <= 600; index += 50)
+            {
+                if (this.ActualHeight >= 1.5 * index)
+                {
+                    resizeCoverArtStoryboard = this.PlaybackInfoPanel.Resources[string.Format("ResizeCoverArt{0}", index)] as Storyboard;
+                }
+            }
+
+            if (resizeCoverArtStoryboard != null) resizeCoverArtStoryboard.Begin();
+        }
+
+        private void UserControl_SizeChanged(object sender, System.Windows.SizeChangedEventArgs e)
+        {
+            this.ResizePlaybackInfo();
+        }
+    }
+}
