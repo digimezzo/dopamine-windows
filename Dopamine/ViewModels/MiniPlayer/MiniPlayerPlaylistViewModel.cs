2e2e029940e6973311d5c90ab6823849e79740a0
Raphaël <raphael.godart@outlook.com>
2017-10-18 10:54:44 +0200

    Added bindings for now playing pages

---
+++ Dopamine/ViewModels/MiniPlayer/MiniPlayerPlaylistViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Prism.Mvvm;
+
+namespace Dopamine.ViewModels.MiniPlayer
+{
+    public class MiniPlayerPlaylistViewModel : BindableBase
+    {
+        private int nowPlayingSelectedPageIndex;
+
+        public int NowPlayingSelectedPageIndex
+        {
+            get { return nowPlayingSelectedPageIndex; }
+            set { SetProperty<int>(ref this.nowPlayingSelectedPageIndex, value); }
+        }
+    }
+}
