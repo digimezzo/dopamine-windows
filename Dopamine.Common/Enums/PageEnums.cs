306e696e1c07804239f88e0b50268b98b6629f0f
Raphaël <raphael.godart@outlook.com>
2017-10-19 22:12:09 +0200

    There's now an enum behind now playing pages

--- Dopamine.Common/Enums/PageEnums.cs
+++ Dopamine.Common/Enums/PageEnums.cs
@@ -10,6 +10,12 @@
         Finish = 5
     }
 
+    public enum NowPlayingPage
+    {
+        NothingPlaying = 0,
+        NowPlaying = 1,
+    }
+
     //public enum SelectedNowPlayingSubPage
     //{
     //    ShowCase = 1,
