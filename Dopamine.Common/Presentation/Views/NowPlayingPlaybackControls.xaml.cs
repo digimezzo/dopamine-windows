bd130a710e71869616932c9532c60740cb6031a4
Raphaël <raphael.godart@outlook.com>
2017-10-20 20:43:12 +0200

    Added an enum converter

--- Dopamine.Common/Presentation/Views/NowPlayingPlaybackControls.xaml.cs
+++ Dopamine.Common/Presentation/Views/NowPlayingPlaybackControls.xaml.cs
@@ -5,18 +5,24 @@ namespace Dopamine.Common.Presentation.Views
 {
     public partial class NowPlayingPlaybackControls : UserControl
     {
-        public int SelectedNowPlayingItemIndex
+        public int SelectedNowPlayingSubPageIndex
         {
-            get { return (int)GetValue(SelectedNowPlayingItemIndexProperty); }
-            set { SetValue(SelectedNowPlayingItemIndexProperty, value); }
+            get { return (int)GetValue(SelectedNowPlayingSubPageIndexProperty); }
+            set { SetValue(SelectedNowPlayingSubPageIndexProperty, value); }
         }
 
-        public static readonly DependencyProperty SelectedNowPlayingItemIndexProperty =
-            DependencyProperty.Register(nameof(SelectedNowPlayingItemIndex), typeof(int), typeof(NowPlayingPlaybackControls), new PropertyMetadata(0));
+        public static readonly DependencyProperty SelectedNowPlayingSubPageIndexProperty =
+            DependencyProperty.Register(nameof(SelectedNowPlayingSubPageIndex), typeof(int), typeof(NowPlayingPlaybackControls), new PropertyMetadata(0, new PropertyChangedCallback(SelectedNowPlayingSubPageIndexPropertyChanged)));
+
+        private static void SelectedNowPlayingSubPageIndexPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            // TODO: save setting
+        }
 
         public NowPlayingPlaybackControls()
         {
             InitializeComponent();
+            // TODO: load from setting
         }
     }
 }
