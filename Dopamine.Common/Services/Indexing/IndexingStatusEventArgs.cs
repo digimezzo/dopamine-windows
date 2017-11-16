5109ad6e984e65aaa35e7bd0f4789e224c7a600f
Raphaël <raphael.godart@outlook.com>
2017-11-16 19:22:37 +0100

    Updated indexing status

--- Dopamine.Common/Services/Indexing/IndexingStatusEventArgs.cs
+++ Dopamine.Common/Services/Indexing/IndexingStatusEventArgs.cs
@@ -6,7 +6,6 @@ namespace Dopamine.Common.Services.Indexing
     {
         public IndexingAction IndexingAction { get; set; }
         public long ProgressCurrent { get; set; }
-        public long ProgressTotal { get; set; }
         public int ProgressPercent { get; set; }
     }
 }
