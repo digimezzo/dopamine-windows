7fa8a1f22fba73b096fa557087726f74e03eb40f
Raphaël <raphael.godart@outlook.com>
2017-12-09 11:37:15 +0100

    Fixed a build issue

--- Dopamine.Common/Services/File/IFileService.cs
+++ Dopamine.Common/Services/File/IFileService.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Dopamine.Common.Services.File
 {
-   public delegate void TracksImportedHandler(List<PlayableTrack> tracks);
+   public delegate void TracksImportedHandler(List<PlayableTrack> tracks, PlayableTrack trackToPlay);
 
     [ServiceContract(Namespace = "http://Dopamine.FileService")]
     public interface IFileService
