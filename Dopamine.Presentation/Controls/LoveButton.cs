6bb16ae3d83caeb5f343a689dbb5cfad04195439
Raphaël <raphael.godart@outlook.com>
2017-12-16 21:54:59 +0100

    Moved LoveButton to Dopamine.Presentation

--- Dopamine.Presentation/Controls/LoveButton.cs
+++ Dopamine.Presentation/Controls/LoveButton.cs
@@ -3,7 +3,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 
-namespace Dopamine.Common.Controls
+namespace Dopamine.Presentation.Controls
 {
     public class LoveButton : Control
     {
