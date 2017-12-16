1bace2f91ab01e02d39168079d621752e88ee792
Raphaël <raphael.godart@outlook.com>
2017-12-16 22:03:40 +0100

    Moved RatingButton to Dopamine.Presentation We're getting there, step by step.

--- Dopamine.Common/Controls/PopupEx.cs
+++ Dopamine.Common/Controls/PopupEx.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Controls.Primitives;
-using System.Reflection;
+﻿using System.Reflection;
 using System.Windows;
+using System.Windows.Controls.Primitives;
 
 namespace Dopamine.Common.Controls
 {
