26e414e48e595ded87fdee2813a7252be98499f7
Raphaël <raphael.godart@outlook.com>
2018-01-01 15:46:46 +0100

    Updated copyright and change log

--- Dopamine/SharedAssemblyInfo.cs
+++ Dopamine/SharedAssemblyInfo.cs
@@ -1,7 +1,7 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyCompany("Digimezzo")]
-[assembly: AssemblyCopyright("Copyright Digimezzo © 2014 - 2017")]
-[assembly: AssemblyVersion("1.5.5.1")]
-[assembly: AssemblyFileVersion("1.5.5.1")]
-[assembly: AssemblyInformationalVersion("1.5.5.1")]
+[assembly: AssemblyCopyright("Copyright Digimezzo © 2014 - 2018")]
+[assembly: AssemblyVersion("1.5.6.0")]
+[assembly: AssemblyFileVersion("1.5.6.0")]
+[assembly: AssemblyInformationalVersion("1.5.6.0")]
