6dc1de33c4dc158adf1fb110b1ed3a874cbbd8b7
Raphaël <raphael.godart@outlook.com>
2018-01-24 08:06:59 +0100

    Added Instagram link

--- Dopamine.Core/Base/ContactInformation.cs
+++ Dopamine.Core/Base/ContactInformation.cs
@@ -7,6 +7,7 @@
         public static string WebsiteContactLink = "http://www.digimezzo.com/contact";
         public static string TwitterLink = "https://twitter.com/digimezzo";
         public static string FacebookLink = "https://www.facebook.com/digimezzo";
+        public static string InstagramLink = "https://www.instagram.com/digimezzo/";
         public static string PayPalLink = "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=MQALEWTEZ7HX8";
     }
 }
