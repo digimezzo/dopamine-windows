5a69dbc4aef5732eda793c8b31d232824d5b7553
Raphaël <raphael.godart@outlook.com>
2018-02-01 18:59:44 +0100

    Updated change log and assembly version

--- Dopamine/SharedAssemblyInfo.cs
+++ Dopamine/SharedAssemblyInfo.cs
@@ -2,6 +2,6 @@
 
 [assembly: AssemblyCompany("Digimezzo")]
 [assembly: AssemblyCopyright("Copyright Digimezzo © 2014 - 2018")]
-[assembly: AssemblyVersion("1.5.9.0")]
-[assembly: AssemblyFileVersion("1.5.9.0")]
-[assembly: AssemblyInformationalVersion("1.5.9.0")]
+[assembly: AssemblyVersion("1.5.10.0")]
+[assembly: AssemblyFileVersion("1.5.10.0")]
+[assembly: AssemblyInformationalVersion("1.5.10.0")]
