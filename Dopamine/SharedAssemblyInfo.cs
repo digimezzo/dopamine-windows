9a9ffdfadfa9987ccc5678a1122b05996140fcac
Raphaël <raphael.godart@outlook.com>
2018-04-15 20:41:43 +0200

    Fixes #703: Re-introduce preview versions, and additionally beta versions

--- Dopamine/SharedAssemblyInfo.cs
+++ Dopamine/SharedAssemblyInfo.cs
@@ -1,7 +1,11 @@
 ﻿using System.Reflection;
-
+// <Major>.<Minor>.<Build>.<Revision>
+// Revision: 1*** = Preview (1001= "Preview 1", 1002 = "Preview 2", ...)
+// Revision: 2*** = Beta (2001= "Beta 1", 2002 = "Beta 2", ...)
+// Revision: 3*** = Release Candidate (3001= "RC 1", 3002 = "RC 2", ...)
+// Revision: 4000 = Release
 [assembly: AssemblyCompany("Digimezzo")]
 [assembly: AssemblyCopyright("Copyright Digimezzo © 2014 - 2018")]
-[assembly: AssemblyVersion("1.5.12.0")]
-[assembly: AssemblyFileVersion("1.5.12.0")]
-[assembly: AssemblyInformationalVersion("1.5.12.0")]
+[assembly: AssemblyVersion("1.5.13.3001")]
+[assembly: AssemblyFileVersion("1.5.13.3001")]
+[assembly: AssemblyInformationalVersion("1.5.13.3001")]
