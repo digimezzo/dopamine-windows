c7bcdaee01d80dbd6113a8e60bf4259dfd451f29
Raphaël <raphael@digimezzo.com>
2018-04-22 11:48:26 +0200

    Added PeekingStreamReader

---
+++ Dopamine.Core/Helpers/PeekingStreamReader.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace Dopamine.Core.Helpers
+{
+    public class PeekingStreamReader : StreamReader
+    {
+        private Queue<string> peeks;
+
+        public PeekingStreamReader(Stream stream) : base(stream)
+        {
+            this.peeks = new Queue<string>();
+        }
+
+        public override string ReadLine()
+        {
+            if (this.peeks.Count > 0)
+            {
+                string nextLine = this.peeks.Dequeue();
+                return nextLine;
+            }
+
+            return base.ReadLine();
+        }
+
+        public string PeekReadLine()
+        {
+            string nextLine = ReadLine();
+            this.peeks.Enqueue(nextLine);
+            return nextLine;
+        }
+    }
+}
