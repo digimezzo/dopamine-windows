4d1b49b218827e592c2ee5b53f6048b6201a069d
Raphaël <raphael.godart@outlook.com>
2018-05-21 21:59:48 +0200

    Removed a using

--- Dopamine.Core/Audio/SpectrumAnalyzer.cs
+++ Dopamine.Core/Audio/SpectrumAnalyzer.cs
@@ -25,7 +25,6 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Shapes;
-using System.Windows.Threading;
 
 namespace Dopamine.Core.Audio
 {
