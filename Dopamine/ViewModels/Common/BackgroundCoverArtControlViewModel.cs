e904816df94625e414018c2561bcbae53410983b
Raphaël <raphael.godart@outlook.com>
2018-05-30 22:45:24 +0200

    More theme tweaking

--- Dopamine/ViewModels/Common/BackgroundCoverArtControlViewModel.cs
+++ Dopamine/ViewModels/Common/BackgroundCoverArtControlViewModel.cs
@@ -1,5 +1,4 @@
-﻿using Digimezzo.Utilities.Settings;
-using Dopamine.Services.Appearance;
+﻿using Dopamine.Services.Appearance;
 using Dopamine.Services.Cache;
 using Dopamine.Services.Metadata;
 using Dopamine.Services.Playback;
@@ -11,14 +10,7 @@ namespace Dopamine.ViewModels.Common
         private IAppearanceService appearanceService;
         private ICacheService cacheService;
         private IMetadataService metadataService;
-        private double opacity;
-   
-        public double Opacity
-        {
-            get { return this.opacity; }
-            set { SetProperty<double>(ref this.opacity, value); }
-        }
-     
+        
         public BackgroundCoverArtControlViewModel(IPlaybackService playbackService,
             ICacheService cacheService, IAppearanceService appearanceService, 
             IMetadataService metadataService) : base(playbackService, cacheService, metadataService)
@@ -27,10 +19,6 @@ namespace Dopamine.ViewModels.Common
             this.appearanceService = appearanceService;
             this.cacheService = cacheService;
             this.metadataService = metadataService;
-
-            this.appearanceService.ThemeChanged += useLightTheme => this.Opacity = useLightTheme ? 1.0 : 0.5;
-
-            this.Opacity = SettingsClient.Get<bool>("Appearance", "EnableLightTheme") ? 1.0 : 0.5;
         }
     }
 }
