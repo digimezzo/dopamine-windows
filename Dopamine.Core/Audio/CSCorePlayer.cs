60d19d850993d1ec842575c414dbbff07feb0c97
Digimezzo <raphael@digimezzo.com>
2018-08-04 12:59:12 +0200

    Small code cleanup

--- Dopamine.Core/Audio/CSCorePlayer.cs
+++ Dopamine.Core/Audio/CSCorePlayer.cs
@@ -49,6 +49,9 @@ namespace Dopamine.Core.Audio
         private bool isPlaying;
         private bool supportsWindowsMediaFoundation = false;
 
+        // To detect redundant calls
+        private bool disposedValue = false;
+
         public CSCorePlayer()
         {
             this.canPlay = true;
@@ -619,38 +622,23 @@ namespace Dopamine.Core.Audio
             }
         }
 
-        private bool disposedValue = false; // To detect redundant calls
-
         protected virtual void Dispose(bool disposing)
         {
             if (!disposedValue)
             {
                 if (disposing)
                 {
-                    // dispose managed state (managed objects).
                     this.CloseSoundOut();
                 }
-
-                // free unmanaged resources (unmanaged objects) and override a finalizer below.
-                // set large fields to null.
 
                 disposedValue = true;
             }
         }
 
-        // override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
-        // ~CSCorePlayer() {
-        //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
-        //   Dispose(false);
-        // }
-
         // This code added to correctly implement the disposable pattern.
         public void Dispose()
         {
-            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
             Dispose(true);
-            // uncomment the following line if the finalizer is overridden above.
-            // GC.SuppressFinalize(this);
         }
     }
 }
