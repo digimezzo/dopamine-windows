3ea582c92cec40a48f4534eeb66f8179445d6f3c
Digimezzo <raphael@digimezzo.com>
2018-08-05 13:33:06 +0200

    FFT code is commented out from ExternalControlServer until we've found a way to make it work without dependency on CSCore.

--- Dopamine.Services/ExternalControl/IFftDataServer.cs
+++ Dopamine.Services/ExternalControl/IFftDataServer.cs
@@ -1,15 +1,15 @@
-﻿using System.ServiceModel;
-using System.Threading.Tasks;
+﻿//using System.ServiceModel;
+//using System.Threading.Tasks;
 
-namespace Dopamine.Services.ExternalControl
-{
-    [ServiceContract(Namespace = nameof(ExternalControl))]
-    public interface IFftDataServer
-    {
-        [OperationContract]
-        int GetFftDataSize();
+//namespace Dopamine.Services.ExternalControl
+//{
+//    [ServiceContract(Namespace = nameof(ExternalControl))]
+//    public interface IFftDataServer
+//    {
+//        [OperationContract]
+//        int GetFftDataSize();
 
-        [OperationContract]
-        Task GetFftData();
-    }
-}
+//        [OperationContract]
+//        Task GetFftData();
+//    }
+//}
