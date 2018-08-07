e217f72ce483139c2809bee1a68506a6f1186aea
Digimezzo <raphael@digimezzo.com>
2018-08-07 20:50:58 +0200

    Fixes #803: Hamburger menu animation not working when OOBE first shown

--- Dopamine/Views/FullPlayer/FullPlayer.xaml.cs
+++ Dopamine/Views/FullPlayer/FullPlayer.xaml.cs
@@ -1,6 +1,9 @@
-﻿using Prism.Regions;
+﻿using Digimezzo.Foundation.WPF.Controls;
+using Prism.Regions;
+using System;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media.Animation;
 
 namespace Dopamine.Views.FullPlayer
 {
@@ -8,20 +11,30 @@ namespace Dopamine.Views.FullPlayer
     {
         private IRegionManager regionManager;
 
-        public bool IsMouseOverSplitViewButton
-        {
-            get { return (bool)GetValue(IsMouseOverSplitViewButtonProperty); }
-            set { SetValue(IsMouseOverSplitViewButtonProperty, value); }
-        }
-
-        public static readonly DependencyProperty IsMouseOverSplitViewButtonProperty =
-            DependencyProperty.Register(nameof(IsMouseOverSplitViewButton), typeof(bool), typeof(FullPlayer), new PropertyMetadata(false));
-
         public FullPlayer(IRegionManager regionManager)
         {
             InitializeComponent();
 
             this.regionManager = regionManager;
+        }
+
+        private void AnimateHamburgerIcon(int newSize, int newOpacity, TimeSpan duration)
+        {
+            DoubleAnimation sizeAnimation = new DoubleAnimation(newSize, duration);
+            DoubleAnimation opacityAnimation = new DoubleAnimation(newOpacity, duration);
+
+            this.HamburgerIcon.BeginAnimation(MaterialIcon.FontSizeProperty, sizeAnimation);
+            this.HamburgerIcon.BeginAnimation(MaterialIcon.OpacityProperty, opacityAnimation);
+        }
+
+        private void AnimateHeadPhoneIcon(int newSize, int newOpacity, TimeSpan duration)
+        {
+            DoubleAnimation sizeAnimation = new DoubleAnimation(newSize, duration);
+            DoubleAnimation opacityAnimation = new DoubleAnimation(newOpacity, duration);
+
+            this.HeadPhoneIcon.BeginAnimation(MaterialIcon.WidthProperty, sizeAnimation);
+            this.HeadPhoneIcon.BeginAnimation(MaterialIcon.HeightProperty, sizeAnimation);
+            this.HeadPhoneIcon.BeginAnimation(MaterialIcon.OpacityProperty, opacityAnimation);
         }
 
         private void UserControl_Loaded(object sender, System.Windows.RoutedEventArgs e)
@@ -43,12 +56,14 @@ namespace Dopamine.Views.FullPlayer
 
         private void Grid_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
         {
-            this.IsMouseOverSplitViewButton = true;
+            this.AnimateHamburgerIcon(24, 1, TimeSpan.FromMilliseconds(250));
+            this.AnimateHeadPhoneIcon(1, 0, TimeSpan.FromMilliseconds(250));
         }
 
         private void Grid_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
         {
-            this.IsMouseOverSplitViewButton = false;
+            this.AnimateHamburgerIcon(1, 0, TimeSpan.FromMilliseconds(250));
+            this.AnimateHeadPhoneIcon(24, 1, TimeSpan.FromMilliseconds(250));
         }
 
         private void InformationButton_Checked(object sender, RoutedEventArgs e)
