cd72ee265e7e8e469e1ee52532c734a9cb5567ee
Raphaël <raphael@digimezzo.com>
2018-08-14 21:29:37 +0200

    Added selection of folder on Folders screen

---
+++ Dopamine.Services/Entities/FolderViewModel.cs
@@ -0,0 +1,56 @@
+﻿using Dopamine.Data.Entities;
+using Prism.Mvvm;
+
+namespace Dopamine.Services.Entities
+{
+    public class FolderViewModel : BindableBase
+    {
+        private Folder folder;
+
+        public FolderViewModel(Folder folder)
+        {
+            this.folder = folder;
+        }
+
+        public Folder Folder => this.folder;
+
+        public string Path => this.folder.Path;
+
+        public string SafePath => this.folder.SafePath;
+
+        public long FolderId => this.folder.FolderID;
+       
+        public string Directory => System.IO.Path.GetFileName(this.folder.Path);
+
+        public bool ShowInCollection
+        {
+            get { return this.folder.ShowInCollection == 1 ? true : false; }
+
+            set
+            {
+                this.folder.ShowInCollection = value ? 1 : 0;
+                RaisePropertyChanged(nameof(this.ShowInCollection));
+            }
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || !GetType().Equals(obj.GetType()))
+            {
+                return false;
+            }
+
+            return string.Equals(this.SafePath, ((FolderViewModel)obj).SafePath);
+        }
+
+        public override int GetHashCode()
+        {
+            return this.SafePath.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return this.Directory; 
+        }
+    }
+}
--- Dopamine/Views/FullPlayer/Collection/CollectionFolders.xaml.cs
+++ Dopamine/Views/FullPlayer/Collection/CollectionFolders.xaml.cs
@@ -1,12 +1,32 @@
-﻿using System.Windows.Controls;
+﻿using Dopamine.Views.Common.Base;
+using System.Windows;
+using System.Windows.Input;
 
 namespace Dopamine.Views.FullPlayer.Collection
 {
-    public partial class CollectionFolders : UserControl
+    public partial class CollectionFolders : TracksViewBase
     {
         public CollectionFolders()
         {
             InitializeComponent();
+        }
+
+        private async void ListBoxTracks_KeyUp(object sender, KeyEventArgs e)
+        {
+            await this.KeyUpHandlerAsync(sender, e);
+        }
+
+        private async void ListBoxTracks_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            await this.ActionHandler(sender, e.OriginalSource as DependencyObject, true);
+        }
+
+        private async void ListBoxTracks_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                await this.ActionHandler(sender, e.OriginalSource as DependencyObject, true);
+            }
         }
     }
 }
