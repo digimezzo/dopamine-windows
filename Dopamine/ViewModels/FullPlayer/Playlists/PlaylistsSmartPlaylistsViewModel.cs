7097b8a802587d55a33475334ac595e03356ccf0
Raphaël <raphael@digimezzo.com>
2018-08-23 22:47:54 +0200

    More refactoring in order to implement smart playlists

--- Dopamine/ViewModels/FullPlayer/Playlists/PlaylistsSmartPlaylistsViewModel.cs
+++ Dopamine/ViewModels/FullPlayer/Playlists/PlaylistsSmartPlaylistsViewModel.cs
@@ -1,13 +1,38 @@
-﻿using Dopamine.ViewModels.Common.Base;
+﻿using Digimezzo.Foundation.Core.Logging;
+using Digimezzo.Utilities.Settings;
+using Dopamine.Services.Entities;
+using Dopamine.Services.Playlist;
+using Dopamine.ViewModels.Common.Base;
 using GongSolutions.Wpf.DragDrop;
 using Prism.Ioc;
+using System;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
 
 namespace Dopamine.ViewModels.FullPlayer.Playlists
 {
-    public class PlaylistsSmartPlaylistsViewModel : TracksViewModelBaseWithTrackArt, IDropTarget
+    public class PlaylistsSmartPlaylistsViewModel : PlaylistsViewModelBase, IDropTarget
     {
-        public PlaylistsSmartPlaylistsViewModel(IContainerProvider container) : base(container)
+        private ISmartPlaylistService smartPlaylistService;
+        private double leftPaneWidthPercent;
+
+        public double LeftPaneWidthPercent
         {
+            get { return this.leftPaneWidthPercent; }
+            set
+            {
+                SetProperty<double>(ref this.leftPaneWidthPercent, value);
+                SettingsClient.Set<int>("ColumnWidths", "SmartPlaylistsLeftPaneWidthPercent", Convert.ToInt32(value));
+            }
+        }
+
+        public PlaylistsSmartPlaylistsViewModel(IContainerProvider container, ISmartPlaylistService smartPlaylistService) : base(container)
+        {
+            // Dependency injection
+            this.smartPlaylistService = smartPlaylistService;
+
+            // Load settings
+            this.LeftPaneWidthPercent = SettingsClient.Get<int>("ColumnWidths", "SmartPlaylistsLeftPaneWidthPercent");
         }
 
         public void DragOver(IDropInfo dropInfo)
@@ -18,6 +43,37 @@ namespace Dopamine.ViewModels.FullPlayer.Playlists
         public void Drop(IDropInfo dropInfo)
         {
             throw new System.NotImplementedException();
+        }
+
+        protected override async Task GetPlaylistsAsync()
+        {
+            try
+            {
+                // Populate an ObservableCollection
+                var playlistViewModels = new ObservableCollection<PlaylistViewModel>(await this.smartPlaylistService.GetSmartPlaylistsAsync());
+
+                // Unbind and rebind to improve UI performance
+                this.Playlists = null;
+                this.Playlists = playlistViewModels;
+            }
+            catch (Exception ex)
+            {
+                LogClient.Error("An error occurred while getting Smart Playlists. Exception: {0}", ex.Message);
+
+                // If loading from the database failed, create and empty Collection.
+                this.Playlists = new ObservableCollection<PlaylistViewModel>();
+            }
+
+            // Notify that the count has changed
+            this.RaisePropertyChanged(nameof(this.PlaylistsCount));
+
+            // Select the firts playlist
+            this.TrySelectFirstPlaylist();
+        }
+
+        protected override async Task GetTracksAsync()
+        {
+            // TODO
         }
     }
 }
