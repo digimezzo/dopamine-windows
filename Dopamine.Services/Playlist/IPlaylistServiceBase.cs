a976bf3ff30262a7253d4eba9b799beeb3d31a23
Raphaël <raphael@digimezzo.com>
2018-08-24 20:18:33 +0200

    More refactoring for smart playlists

---
+++ Dopamine.Services/Playlist/IPlaylistServiceBase.cs
@@ -0,0 +1,27 @@
+﻿using Dopamine.Services.Entities;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Dopamine.Services.Playlist
+{
+    public interface IPlaylistServiceBase
+    {
+        string PlaylistFolder { get; }
+
+        Task<AddPlaylistResult> AddPlaylistAsync(string playlistName);
+
+        Task<DeletePlaylistsResult> DeletePlaylistAsync(string playlistName);
+
+        Task<RenamePlaylistResult> RenamePlaylistAsync(string oldPlaylistName, string newPlaylistName);
+
+        Task<IList<PlaylistViewModel>> GetPlaylistsAsync();
+
+        Task<ImportPlaylistResult> ImportPlaylistsAsync(IList<string> fileNames);
+
+        event PlaylistAddedHandler PlaylistAdded;
+        event PlaylistDeletedHandler PlaylistDeleted;
+        event PlaylistRenamedHandler PlaylistRenamed;
+        event EventHandler PlaylistFolderChanged;
+    }
+}
---
+++ Dopamine.Services/Playlist/PlaylistEventHandlers.cs
@@ -0,0 +1,10 @@
+﻿using Dopamine.Services.Entities;
+
+namespace Dopamine.Services.Playlist
+{
+    public delegate void PlaylistAddedHandler(PlaylistViewModel addedPlaylist);
+    public delegate void TracksAddedHandler(int numberTracksAdded, string playlistName);
+    public delegate void TracksDeletedHandler(string playlistName);
+    public delegate void PlaylistDeletedHandler(PlaylistViewModel deletedPlaylist);
+    public delegate void PlaylistRenamedHandler(PlaylistViewModel oldPlaylist, PlaylistViewModel newPlaylist);
+}
