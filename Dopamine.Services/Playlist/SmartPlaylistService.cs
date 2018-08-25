07b5bdb7057972bb84d571600517c4f059282e20
Raphaël <raphael@digimezzo.com>
2018-08-25 09:31:41 +0200

    Added getting of smart playlists

---
+++ Dopamine.Services/Playlist/SmartPlaylistService.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Xml.Linq;
+using Digimezzo.Utilities.Log;
+using Dopamine.Core.Base;
+using Dopamine.Core.Helpers;
+using Dopamine.Services.Entities;
+
+namespace Dopamine.Services.Playlist
+{
+    public class SmartPlaylistService : ISmartPlaylistService
+    {
+        private GentleFolderWatcher watcher;
+
+        public SmartPlaylistService()
+        {
+            // Initialize Playlists folder
+            string musicFolder = Environment.GetFolderPath(Environment.SpecialFolder.MyMusic);
+            this.PlaylistFolder = Path.Combine(musicFolder, ProductInformation.ApplicationName, "Smart playlists");
+
+            if (!Directory.Exists(this.PlaylistFolder))
+            {
+                try
+                {
+                    Directory.CreateDirectory(this.PlaylistFolder);
+                }
+                catch (Exception ex)
+                {
+                    LogClient.Error("Could not create Smart playlists folder. Exception: {0}", ex.Message);
+                }
+            }
+
+            // Watcher
+            this.watcher = new GentleFolderWatcher(this.PlaylistFolder, false);
+            this.watcher.FolderChanged += Watcher_FolderChanged;
+            this.watcher.Resume();
+        }
+
+        private void Watcher_FolderChanged(object sender, EventArgs e)
+        {
+            Application.Current.Dispatcher.Invoke(() =>
+            {
+                this.PlaylistFolderChanged(this, new EventArgs());
+            });
+        }
+
+        public string PlaylistFolder { get; }
+
+        public event PlaylistAddedHandler PlaylistAdded = delegate { };
+        public event PlaylistDeletedHandler PlaylistDeleted = delegate { };
+        public event PlaylistRenamedHandler PlaylistRenamed = delegate { };
+        public event EventHandler PlaylistFolderChanged = delegate { };
+
+        private string GetSmartPlaylistName(string smartPlaylistPath)
+        {
+            string name = string.Empty;
+
+            try
+            {
+                XDocument xdoc = XDocument.Load(smartPlaylistPath);
+
+                XElement nameElement = (from t in xdoc.Element("smartplaylist").Elements("name")
+                                        select t).FirstOrDefault();
+
+                name = nameElement.Value;
+            }
+            catch (Exception ex)
+            {
+                LogClient.Error($"Could not get name for smart playlist '{smartPlaylistPath}'. Exception: {ex.Message}");
+            }
+
+            return name;
+        }
+
+        public async Task<IList<PlaylistViewModel>> GetPlaylistsAsync()
+        {
+            IList<PlaylistViewModel> playlists = new List<PlaylistViewModel>();
+
+            await Task.Run(() =>
+            {
+                try
+                {
+                    var di = new DirectoryInfo(this.PlaylistFolder);
+                    var fi = di.GetFiles("*" + FileFormats.DSPL, SearchOption.TopDirectoryOnly);
+
+                    foreach (FileInfo f in fi)
+                    {
+                        string name = this.GetSmartPlaylistName(f.FullName);
+
+                        if (!string.IsNullOrEmpty(name))
+                        {
+                            playlists.Add(new PlaylistViewModel(name, f.FullName));
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    LogClient.Error("Error while getting smart playlists. Exception: {0}", ex.Message);
+                }
+            });
+
+            return playlists;
+        }
+
+        public Task<AddPlaylistResult> AddPlaylistAsync(string playlistName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<DeletePlaylistsResult> DeletePlaylistAsync(string playlistName)
+        {
+            throw new NotImplementedException();
+        }
+
+        
+
+        public Task<ImportPlaylistResult> ImportPlaylistsAsync(IList<string> fileNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<RenamePlaylistResult> RenamePlaylistAsync(string oldPlaylistName, string newPlaylistName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Dopamine/ViewModels/FullPlayer/Playlists/PlaylistsSmartPlaylistsViewModel.cs
+++ Dopamine/ViewModels/FullPlayer/Playlists/PlaylistsSmartPlaylistsViewModel.cs
@@ -1,20 +1,36 @@
-﻿using Digimezzo.Utilities.Settings;
+﻿using Digimezzo.Utilities.Log;
+using Digimezzo.Utilities.Settings;
+using Dopamine.Services.Entities;
+using Dopamine.Services.Playlist;
 using Dopamine.ViewModels.Common.Base;
 using GongSolutions.Wpf.DragDrop;
 using Prism.Ioc;
 using System;
+using System.Collections.ObjectModel;
 using System.Threading.Tasks;
 
 namespace Dopamine.ViewModels.FullPlayer.Playlists
 {
     public class PlaylistsSmartPlaylistsViewModel : PlaylistsViewModelBase, IDropTarget
     {
+        private ISmartPlaylistService smartPlaylistService;
         private double leftPaneWidthPercent;
 
-        public PlaylistsSmartPlaylistsViewModel(IContainerProvider container) : base(container)
+        public PlaylistsSmartPlaylistsViewModel(IContainerProvider container, ISmartPlaylistService smartPlaylistService) : base(container)
         {
+            // Dependency injection
+            this.smartPlaylistService = smartPlaylistService;
+
+            // Events
+            this.smartPlaylistService.PlaylistFolderChanged += SmartPlaylistService_PlaylistFolderChanged; ;
+
             // Load settings
             this.LeftPaneWidthPercent = SettingsClient.Get<int>("ColumnWidths", "SmartPlaylistsLeftPaneWidthPercent");
+        }
+
+        private async void SmartPlaylistService_PlaylistFolderChanged(object sender, EventArgs e)
+        {
+            await this.FillListsAsync();
         }
 
         public double LeftPaneWidthPercent
@@ -39,7 +55,28 @@ namespace Dopamine.ViewModels.FullPlayer.Playlists
 
         protected override async Task GetPlaylistsAsync()
         {
-           // TODO
+            try
+            {
+                // Populate an ObservableCollection
+                var playlistViewModels = new ObservableCollection<PlaylistViewModel>(await this.smartPlaylistService.GetPlaylistsAsync());
+
+                // Unbind and rebind to improve UI performance
+                this.Playlists = null;
+                this.Playlists = playlistViewModels;
+            }
+            catch (Exception ex)
+            {
+                LogClient.Error("An error occurred while getting Smart Playlists. Exception: {0}", ex.Message);
+
+                // If loading from the database failed, create and empty Collection.
+                this.Playlists = new ObservableCollection<PlaylistViewModel>();
+            }
+
+            // Notify that the count has changed
+            this.RaisePropertyChanged(nameof(this.PlaylistsCount));
+
+            // Select the firts playlist
+            this.TrySelectFirstPlaylist();
         }
 
         protected override async Task GetTracksAsync()
