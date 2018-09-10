173012de419940ceac32cc3918aa38228198631a
Raphaël <raphael@digimezzo.com>
2018-09-10 19:33:00 +0200

    Added placeholder dialog for smart playlists

---
+++ Dopamine/ViewModels/FullPlayer/Playlists/PlaylistsSmartPlaylistsCreateViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Prism.Mvvm;
+using System.Threading.Tasks;
+
+namespace Dopamine.ViewModels.FullPlayer.Playlists
+{
+    public class PlaylistsSmartPlaylistsCreateViewModel : BindableBase
+    {
+        public async Task<bool> SaveSmartPlaylistAsync()
+        {
+            await Task.Delay(20);
+            return true;
+        }
+    }
+}
