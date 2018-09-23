9b0bf3bf098fecf9d58dddf841e43875a815edb9
Raphaël <raphael@digimezzo.com>
2018-09-23 13:22:14 +0200

    Building the smart playlist window

--- Dopamine.Core/IO/SmartPlaylistdecoder.cs
+++ Dopamine.Core/IO/SmartPlaylistdecoder.cs
@@ -1,7 +1,6 @@
 ﻿using Digimezzo.Utilities.Helpers;
 using Digimezzo.Utilities.Log;
 using Dopamine.Core.Base;
-using Dopamine.Core.Utils;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,10 +20,10 @@ namespace Dopamine.Core.IO
 
         public string Limit { get; set; }
 
-        public IList<Rule> Rules { get; set; }
+        public IList<SmartPlaylistRule> Rules { get; set; }
     }
 
-    public class Rule
+    public class SmartPlaylistRule
     {
         public string Field { get; private set; }
 
@@ -32,7 +31,7 @@ namespace Dopamine.Core.IO
 
         public string Value { get; private set; }
 
-        public Rule(string field, string @operator, string value)
+        public SmartPlaylistRule(string field, string @operator, string value)
         {
             this.Field = field;
             this.Operator = @operator;
@@ -55,7 +54,7 @@ namespace Dopamine.Core.IO
             string match = string.Empty;
             string order = string.Empty;
             string limit = string.Empty;
-            IList<Rule> rules = new List<Rule>();
+            IList<SmartPlaylistRule> rules = new List<SmartPlaylistRule>();
 
             try
             {
@@ -96,7 +95,7 @@ namespace Dopamine.Core.IO
 
                 foreach (XElement ruleElement in ruleElements)
                 {
-                    rules.Add(new Rule(ruleElement.Attribute("field").Value, ruleElement.Attribute("operator").Value, ruleElement.Value));
+                    rules.Add(new SmartPlaylistRule(ruleElement.Attribute("field").Value, ruleElement.Attribute("operator").Value, ruleElement.Value));
                 }
             }
             catch (Exception ex)
@@ -114,298 +113,6 @@ namespace Dopamine.Core.IO
                 Limit = limit,
                 Rules = rules
             };
-        }
-
-        public string GetWhereOperator(string playlistMatch)
-        {
-            string whereOperator = string.Empty;
-
-            switch (playlistMatch)
-            {
-                case "any":
-                    whereOperator = "OR";
-                    break;
-                case "all":
-                default:
-                    whereOperator = "AND";
-                    break;
-            }
-
-            return whereOperator;
-        }
-
-        public string GetLimit(string playlistLimit)
-        {
-            string limit = string.Empty;
-
-            long parsedLong = 0;
-
-            if (long.TryParse(playlistLimit, out parsedLong) && parsedLong > 0)
-            {
-                limit = $"LIMIT {parsedLong}";
-            }
-
-            return limit;
-        }
-
-        public string GetOrder(string playlistOrder)
-        {
-            string sqlOrder = string.Empty;
-
-            switch (playlistOrder)
-            {
-                case "descending":
-                    sqlOrder = "DESC";
-                    break;
-                case "ascending":
-                default:
-                    sqlOrder = "ASC";
-                    break;
-            }
-
-            return sqlOrder;
-        }
-
-        public string GetWhereClausePart(Rule rule)
-        {
-            string whereSubClause = string.Empty;
-
-            // Artist
-            if (rule.Field.Equals("artist", StringComparison.InvariantCultureIgnoreCase))
-            {
-                if (rule.Operator.Equals("is"))
-                {
-                    whereSubClause = $"Artists LIKE '%{FormatUtils.DelimitValue(rule.Value)}%'";
-                }
-                else if (rule.Operator.Equals("contains"))
-                {
-                    whereSubClause = $"Artists LIKE '%{rule.Value}%'";
-                }
-            }
-
-            // AlbumArtist
-            if (rule.Field.Equals("albumartist", StringComparison.InvariantCultureIgnoreCase))
-            {
-                if (rule.Operator.Equals("is"))
-                {
-                    whereSubClause = $"AlbumArtists LIKE '%{FormatUtils.DelimitValue(rule.Value)}%'";
-                }
-                else if (rule.Operator.Equals("contains"))
-                {
-                    whereSubClause = $"AlbumArtists LIKE '%{rule.Value}%'";
-                }
-            }
-
-            // Genre
-            if (rule.Field.Equals("genre", StringComparison.InvariantCultureIgnoreCase))
-            {
-                if (rule.Operator.Equals("is"))
-                {
-                    whereSubClause = $"Genres LIKE '%{FormatUtils.DelimitValue(rule.Value)}%'";
-                }
-                else if (rule.Operator.Equals("contains"))
-                {
-                    whereSubClause = $"Genres LIKE '%{rule.Value}%'";
-                }
-            }
-
-            // Title
-            if (rule.Field.Equals("title", StringComparison.InvariantCultureIgnoreCase))
-            {
-                if (rule.Operator.Equals("is"))
-                {
-                    whereSubClause = $"TrackTitle = '{rule.Value}'";
-                }
-                else if (rule.Operator.Equals("contains"))
-                {
-                    whereSubClause = $"TrackTitle LIKE '%{rule.Value}%'";
-                }
-            }
-
-            // AlbumTitle
-            if (rule.Field.Equals("albumtitle", StringComparison.InvariantCultureIgnoreCase))
-            {
-                if (rule.Operator.Equals("is"))
-                {
-                    whereSubClause = $"AlbumTitle = '{rule.Value}'";
-                }
-                else if (rule.Operator.Equals("contains"))
-                {
-                    whereSubClause = $"AlbumTitle LIKE '%{rule.Value}%'";
-                }
-            }
-
-            // BitRate
-            if (rule.Field.Equals("bitrate", StringComparison.InvariantCultureIgnoreCase))
-            {
-                if (rule.Operator.Equals("is"))
-                {
-                    whereSubClause = $"BitRate = {rule.Value}";
-                }
-                else if (rule.Operator.Equals("greaterthan"))
-                {
-                    whereSubClause = $"BitRate > {rule.Value}";
-                }
-                else if (rule.Operator.Equals("lessthan"))
-                {
-                    whereSubClause = $"BitRate < {rule.Value}";
-                }
-            }
-
-            // TrackNumber
-            if (rule.Field.Equals("tracknumber", StringComparison.InvariantCultureIgnoreCase))
-            {
-                if (rule.Operator.Equals("is"))
-                {
-                    whereSubClause = $"TrackNumber = {rule.Value}";
-                }
-                else if (rule.Operator.Equals("greaterthan"))
-                {
-                    whereSubClause = $"TrackNumber > {rule.Value}";
-                }
-                else if (rule.Operator.Equals("lessthan"))
-                {
-                    whereSubClause = $"TrackNumber < {rule.Value}";
-                }
-            }
-
-            // TrackCount
-            if (rule.Field.Equals("trackcount", StringComparison.InvariantCultureIgnoreCase))
-            {
-                if (rule.Operator.Equals("is"))
-                {
-                    whereSubClause = $"TrackCount = {rule.Value}";
-                }
-                else if (rule.Operator.Equals("greaterthan"))
-                {
-                    whereSubClause = $"TrackCount > {rule.Value}";
-                }
-                else if (rule.Operator.Equals("lessthan"))
-                {
-                    whereSubClause = $"TrackCount < {rule.Value}";
-                }
-            }
-
-            // DiscNumber
-            if (rule.Field.Equals("discnumber", StringComparison.InvariantCultureIgnoreCase))
-            {
-                if (rule.Operator.Equals("is"))
-                {
-                    whereSubClause = $"DiscNumber = {rule.Value}";
-                }
-                else if (rule.Operator.Equals("greaterthan"))
-                {
-                    whereSubClause = $"DiscNumber > {rule.Value}";
-                }
-                else if (rule.Operator.Equals("lessthan"))
-                {
-                    whereSubClause = $"DiscNumber < {rule.Value}";
-                }
-            }
-
-            // DiscCount
-            if (rule.Field.Equals("disccount", StringComparison.InvariantCultureIgnoreCase))
-            {
-                if (rule.Operator.Equals("is"))
-                {
-                    whereSubClause = $"DiscCount = {rule.Value}";
-                }
-                else if (rule.Operator.Equals("greaterthan"))
-                {
-                    whereSubClause = $"DiscCount > {rule.Value}";
-                }
-                else if (rule.Operator.Equals("lessthan"))
-                {
-                    whereSubClause = $"DiscCount < {rule.Value}";
-                }
-            }
-
-            // Year
-            if (rule.Field.Equals("year", StringComparison.InvariantCultureIgnoreCase))
-            {
-                if (rule.Operator.Equals("is"))
-                {
-                    whereSubClause = $"Year = {rule.Value}";
-                }
-                else if (rule.Operator.Equals("greaterthan"))
-                {
-                    whereSubClause = $"Year > {rule.Value}";
-                }
-                else if (rule.Operator.Equals("lessthan"))
-                {
-                    whereSubClause = $"Year < {rule.Value}";
-                }
-            }
-
-            // Rating
-            if (rule.Field.Equals("rating", StringComparison.InvariantCultureIgnoreCase))
-            {
-                if (rule.Operator.Equals("is"))
-                {
-                    whereSubClause = $"Rating = {rule.Value}";
-                }
-                else if (rule.Operator.Equals("greaterthan"))
-                {
-                    whereSubClause = $"Rating > {rule.Value}";
-                }
-                else if (rule.Operator.Equals("lessthan"))
-                {
-                    whereSubClause = $"Rating < {rule.Value}";
-                }
-            }
-
-            // Love
-            if (rule.Field.Equals("love", StringComparison.InvariantCultureIgnoreCase))
-            {
-                if (rule.Operator.Equals("is"))
-                {
-                    whereSubClause = $"Love = {rule.Value}";
-                }
-                else if (rule.Operator.Equals("greaterthan"))
-                {
-                    whereSubClause = $"Love > {rule.Value}";
-                }
-                else if (rule.Operator.Equals("lessthan"))
-                {
-                    whereSubClause = $"Love < {rule.Value}";
-                }
-            }
-
-            // PlayCount
-            if (rule.Field.Equals("playcount", StringComparison.InvariantCultureIgnoreCase))
-            {
-                if (rule.Operator.Equals("is"))
-                {
-                    whereSubClause = $"PlayCount = {rule.Value}";
-                }
-                else if (rule.Operator.Equals("greaterthan"))
-                {
-                    whereSubClause = $"PlayCount > {rule.Value}";
-                }
-                else if (rule.Operator.Equals("lessthan"))
-                {
-                    whereSubClause = $"PlayCount < {rule.Value}";
-                }
-            }
-
-            // SkipCount
-            if (rule.Field.Equals("skipcount", StringComparison.InvariantCultureIgnoreCase))
-            {
-                if (rule.Operator.Equals("is"))
-                {
-                    whereSubClause = $"SkipCount = {rule.Value}";
-                }
-                else if (rule.Operator.Equals("greaterthan"))
-                {
-                    whereSubClause = $"SkipCount > {rule.Value}";
-                }
-                else if (rule.Operator.Equals("lessthan"))
-                {
-                    whereSubClause = $"SkipCount < {rule.Value}";
-                }
-            }
-
-            return whereSubClause;
         }
     }
 }
--- Dopamine/ViewModels/FullPlayer/Collection/CollectionPlaylistsCreatorViewModel.cs
+++ Dopamine/ViewModels/FullPlayer/Collection/CollectionPlaylistsCreatorViewModel.cs
@@ -1,7 +1,9 @@
 ﻿using Digimezzo.Utilities.Utils;
+using Dopamine.Services.Entities;
 using Dopamine.Services.Playlist;
 using Prism.Commands;
 using Prism.Mvvm;
+using System.Collections.ObjectModel;
 
 namespace Dopamine.ViewModels.FullPlayer.Collection
 {
@@ -10,14 +12,28 @@ namespace Dopamine.ViewModels.FullPlayer.Collection
         private PlaylistType playlistType;
         private IPlaylistService playlistService;
         private string playlistName;
-
-        public DelegateCommand LoadedCommand { get; set; }
+        private ObservableCollection<SmartPlaylistRuleViewModel> rules;
 
         public CollectionPlaylistsCreatorViewModel(IPlaylistService playlistService)
         {
             this.playlistService = playlistService;
 
             this.LoadedCommand = new DelegateCommand(() => this.LoadedAsync());
+            this.AddRuleCommand = new DelegateCommand(() => this.AddRule());
+            this.RemoveRuleCommand = new DelegateCommand<SmartPlaylistRuleViewModel>((rule) => this.RemoveRule(rule));
+            this.AddFirstRule();
+        }
+
+        public DelegateCommand LoadedCommand { get; set; }
+
+        public DelegateCommand AddRuleCommand { get; set; }
+
+        public DelegateCommand<SmartPlaylistRuleViewModel> RemoveRuleCommand { get; set; }
+
+        public ObservableCollection<SmartPlaylistRuleViewModel> Rules
+        {
+            get { return this.rules; }
+            set { SetProperty<ObservableCollection<SmartPlaylistRuleViewModel>>(ref this.rules, value); }
         }
 
         private async void LoadedAsync()
@@ -35,6 +51,22 @@ namespace Dopamine.ViewModels.FullPlayer.Collection
         {
             get { return this.playlistName; }
             set { SetProperty<string>(ref this.playlistName, value); }
+        }
+
+        private void AddFirstRule()
+        {
+            this.Rules = new ObservableCollection<SmartPlaylistRuleViewModel>();
+            this.Rules.Add(new SmartPlaylistRuleViewModel());
+        }
+
+        private void AddRule()
+        {
+            this.Rules.Add(new SmartPlaylistRuleViewModel());
+        }
+
+        private void RemoveRule(SmartPlaylistRuleViewModel rule)
+        {
+            this.Rules.Remove(rule);
         }
     }
 }
