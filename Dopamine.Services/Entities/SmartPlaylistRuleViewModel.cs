4b885353a75eedeed2792fa7714be5dd8a7ea5ba
Raphaël <raphael@digimezzo.com>
2018-10-05 08:05:58 +0200

    Added some ViewModels for the smart playlist editor

--- Dopamine.Services/Entities/SmartPlaylistRuleViewModel.cs
+++ Dopamine.Services/Entities/SmartPlaylistRuleViewModel.cs
@@ -4,5 +4,26 @@ namespace Dopamine.Services.Entities
 {
     public class SmartPlaylistRuleViewModel : BindableBase
     {
+        private string field;
+        private string @operator;
+        private string value;
+
+        public string Field
+        {
+            get { return this.field; }
+            set { SetProperty<string>(ref this.field, value); }
+        }
+
+        public string Operator
+        {
+            get { return this.@operator; }
+            set { SetProperty<string>(ref this.@operator, value); }
+        }
+
+        public string Value
+        {
+            get { return this.value; }
+            set { SetProperty<string>(ref this.value, value); }
+        }
     }
 }
