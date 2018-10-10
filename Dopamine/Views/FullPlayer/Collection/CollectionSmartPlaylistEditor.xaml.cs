b7f3306692e99e1799f880d62ef25d64df172076
Raphaël <raphael@digimezzo.com>
2018-10-10 21:28:08 +0200

    Moved smart playlist editor to a user control

---
+++ Dopamine/Views/FullPlayer/Collection/CollectionSmartPlaylistEditor.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace Dopamine.Views.FullPlayer.Collection
+{
+    public partial class CollectionSmartPlaylistEditor : UserControl
+    {
+        public CollectionSmartPlaylistEditor()
+        {
+            InitializeComponent();
+        }
+    }
+}
