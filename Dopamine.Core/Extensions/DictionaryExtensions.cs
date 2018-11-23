8810daa2f1545a71df5dc264b24ad274fcba4ba7
Raphaël <raphael@digimezzo.com>
2018-11-23 10:35:23 +0100

    Fixes #869: Track numbers are sorted and played "alphabetically". Pesky humans and their "wrong" sort orders.

--- Dopamine.Core/Extensions/DictionaryExtensions.cs
+++ Dopamine.Core/Extensions/DictionaryExtensions.cs
@@ -1,13 +1,33 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace Dopamine.Core.Extensions
 {
-    public static class DictionaryExtensions
+    public static class CollectionExtensions
     {
         public static void TryRemove<TKey, TValue>(this Dictionary<TKey, TValue> dict, TKey key)
         {
             if (dict.ContainsKey(key))
                 dict.Remove(key);
+        }
+
+        /// <summary>
+        /// Sorts strings in a "natural" way (taking numbers into account). Humans like that better.
+        /// See: https://stackoverflow.com/questions/248603/natural-sort-order-in-c-sharp
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="selector"></param>
+        /// <returns></returns>
+        public static IOrderedEnumerable<T> OrderByAlphaNumeric<T>(this IEnumerable<T> source, Func<T, string> selector)
+        {
+            int max = source
+                .SelectMany(i => Regex.Matches(selector(i), @"\d+").Cast<Match>().Select(m => (int?)m.Value.Length))
+                .Max() ?? 0;
+
+            return source.OrderBy(i => Regex.Replace(selector(i), @"\d+", m => m.Value.PadLeft(max, '0')));
         }
     }
 }
