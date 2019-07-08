3787e73f168fb2d0cecb2236ba2512223ff73da4
Raphaël <raphael@digimezzo.com>
2019-07-08 23:51:40 +0200

    Fixed a crash

---
+++ Dopamine.Services/Playback/ProgressChangedEventArgs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Dopamine.Services.Playback
+{
+    public class ProgressChangedEventArgs : EventArgs
+    {
+        public TimeSpan CurrentTime { get; set; }
+
+        public TimeSpan TotalTime { get; set; }
+
+        public double Progress { get; set; }
+    }
+}
