856612c0be24bf870a29a34a69399b8136a6af34
Raphaël <raphael@digimezzo.com>
2019-12-14 20:09:59 +0100

    Fixes #962: can't add songs to playlist in now playing plane

---
+++ Dopamine/ViewModels/NowPlaying/NowPlayingPlaylistViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Dopamine.ViewModels.Common.Base;
+using Prism.Ioc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dopamine.ViewModels.NowPlaying
+{
+    public class NowPlayingPlaylistViewModel : ContextMenuViewModelBase
+    {
+        public NowPlayingPlaylistViewModel(IContainerProvider container) : base(container)
+        {
+        }
+
+        protected override void SearchOnline(string id)
+        {
+            // No implementation required here
+        }
+    }
+}
