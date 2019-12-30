cbd7e703329dd32c5e99f480c83ab5998c845e66
Raphaël <raphael@digimezzo.com>
2019-12-30 09:42:49 +0100

    Fixes #1045: External Control API CommunicationException with GetCurrenTrack()

--- Dopamine.Services/ExternalControl/ExternalTrack.cs
+++ Dopamine.Services/ExternalControl/ExternalTrack.cs
@@ -1,68 +1,70 @@
 ﻿using Dopamine.Services.Entities;
-using System;
-using System.Runtime.Serialization;
 
 namespace Dopamine.Services.ExternalControl
 {
     /// <summary>
     /// ExternalControlServer can't use TrackViewModel directly, as it is not serializable.
     /// </summary>
-    [DataContract]
     public class ExternalTrack
     {
         private TrackViewModel trackViewModel;
 
+        public ExternalTrack()
+        {
+        }
+
         public ExternalTrack(TrackViewModel trackViewModel)
         {
             this.trackViewModel = trackViewModel;
+
+            this.BitRate = this.trackViewModel?.Track?.BitRate != null ? this.trackViewModel.Track.BitRate.Value : 0;
+            this.Duration = this.trackViewModel?.Track?.Duration != null ? this.trackViewModel.Track.Duration.Value : 0;
+            this.FileName = !string.IsNullOrEmpty(this.trackViewModel.Track.FileName) ? this.trackViewModel.Track.FileName : "";
+            this.Path = !string.IsNullOrEmpty(this.trackViewModel.Track.Path) ? this.trackViewModel.Track.Path : "";
+            this.SampleRate = this.trackViewModel?.Track?.SampleRate != null ? this.trackViewModel.Track.SampleRate.Value : 0;
+            this.TrackNumber = this.trackViewModel?.Track?.TrackNumber != null ? this.trackViewModel.Track.TrackNumber.Value.ToString() : "";
+            this.TrackTitle = !string.IsNullOrEmpty(this.trackViewModel.TrackTitle) ? this.trackViewModel.TrackTitle : "";
+            this.Year = !string.IsNullOrEmpty(this.trackViewModel.Year) ? this.trackViewModel.Year : "";
+            this.AlbumArtist = !string.IsNullOrEmpty(this.trackViewModel.AlbumArtist) ? this.trackViewModel.AlbumArtist : "";
+            this.AlbumTitle = !string.IsNullOrEmpty(this.trackViewModel.AlbumTitle) ? this.trackViewModel.AlbumTitle : "";
+            this.ArtistName = !string.IsNullOrEmpty(this.trackViewModel.ArtistName) ? this.trackViewModel.ArtistName : "";
+            this.Genre = !string.IsNullOrEmpty(this.trackViewModel.Genre) ? this.trackViewModel.Genre : "";
+            this.Love = this.trackViewModel?.Track?.Love != null ? this.trackViewModel.Track.Love.Value : 0;
+            this.PlayCount = this.trackViewModel?.Track?.PlayCount != null ? this.trackViewModel.Track.PlayCount.Value : 0;
+            this.Rating = this.trackViewModel?.Track?.Rating != null ? this.trackViewModel.Track.Rating.Value : 0;
+            this.SkipCount = this.trackViewModel?.Track?.SkipCount != null ? this.trackViewModel.Track.SkipCount.Value : 0;
         }
 
-        [DataMember]
-        public long BitRate => this.trackViewModel.Track.BitRate.Value;
+        public long BitRate { get; set; }
 
-        [DataMember]
-        public long Duration => this.trackViewModel.Track.Duration.Value;
+        public long Duration { get; set; }
 
-        [DataMember]
-        public string FileName => this.trackViewModel.Track.FileName;
+        public string FileName { get; set; }
 
-        [DataMember]
-        public string Path => this.trackViewModel.Track.Path;
+        public string Path { get; set; }
 
-        [DataMember]
-        public long SampleRate => this.trackViewModel.Track.SampleRate.Value;
+        public long SampleRate { get; set; }
 
-        [DataMember]
-        public string TrackNumber => this.trackViewModel.TrackNumber;
+        public string TrackNumber { get; set; }
 
-        [DataMember]
-        public string TrackTitle => this.trackViewModel.TrackTitle;
+        public string TrackTitle { get; set; }
 
-        [DataMember]
-        public string Year => this.trackViewModel.Year;
+        public string Year { get; set; }
 
-        [DataMember]
-        public string AlbumArtist => this.trackViewModel.AlbumArtist;
+        public string AlbumArtist { get; set; }
 
-        [DataMember]
-        public string AlbumTitle => this.trackViewModel.AlbumTitle;
+        public string AlbumTitle { get; set; }
 
-        [DataMember]
-        public string ArtistName => this.trackViewModel.ArtistName;
+        public string ArtistName { get; set; }
 
-        [DataMember]
-        public string Genre => this.trackViewModel.Genre;
+        public string Genre { get; set; }
 
-        [DataMember]
-        public long Love => this.trackViewModel.Track.Love.Value;
+        public long Love { get; set; }
 
-        [DataMember]
-        public long PlayCount => this.trackViewModel.Track.PlayCount.Value;
+        public long PlayCount { get; set; }
 
-        [DataMember]
-        public long Rating => this.trackViewModel.Track.Rating.Value;
+        public long Rating { get; set; }
 
-        [DataMember]
-        public long SkipCount => this.trackViewModel.Track.SkipCount.Value;
+        public long SkipCount { get; set; }
     }
 }
