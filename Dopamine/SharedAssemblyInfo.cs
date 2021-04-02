f1111f201186d511045416f6029cd8036ba425c3
Digimezzo <raphael@digimezzo.com>
2021-04-02 21:44:54 +0200

    Updated assembly version and copyright

--- Dopamine/SharedAssemblyInfo.cs
+++ Dopamine/SharedAssemblyInfo.cs
@@ -5,7 +5,7 @@
 // Revision: 3*** = Release Candidate (3001= "RC 1", 3002 = "RC 2", ...)
 // Revision: 4000 = Release
 [assembly: AssemblyCompany("Digimezzo")]
-[assembly: AssemblyCopyright("Copyright Digimezzo © 2014 - 2020")]
-[assembly: AssemblyVersion("2.0.5.4000")]
-[assembly: AssemblyFileVersion("2.0.5.4000")]
-[assembly: AssemblyInformationalVersion("2.0.5.4000")]
+[assembly: AssemblyCopyright("Copyright Digimezzo © 2014 - 2021")]
+[assembly: AssemblyVersion("2.0.6.4000")]
+[assembly: AssemblyFileVersion("2.0.6.4000")]
+[assembly: AssemblyInformationalVersion("2.0.6.4000")]
