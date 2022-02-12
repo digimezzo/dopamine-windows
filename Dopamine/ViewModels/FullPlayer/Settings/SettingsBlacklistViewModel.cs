8f450b0d16fa41b35fe8793a01ba977a1b5b0e6d
Digimezzo Raphaël <raphael@digimezzo.com>
2022-02-12 19:22:21 +0100

    We can now remove songs from the blacklist

--- Dopamine/ViewModels/FullPlayer/Settings/SettingsBlacklistViewModel.cs
+++ Dopamine/ViewModels/FullPlayer/Settings/SettingsBlacklistViewModel.cs
@@ -1,9 +1,11 @@
 ﻿using Digimezzo.Foundation.Core.IO;
 using Digimezzo.Foundation.Core.Logging;
 using Digimezzo.Foundation.Core.Settings;
+using Digimezzo.Foundation.Core.Utils;
 using Dopamine.Core.Base;
 using Dopamine.Core.IO;
 using Dopamine.Services.Blacklist;
+using Dopamine.Services.Dialog;
 using Dopamine.Services.Entities;
 using Dopamine.Services.Playback;
 using Prism.Commands;
@@ -18,20 +20,32 @@ namespace Dopamine.ViewModels.FullPlayer.Settings
     public class SettingsBlacklistViewModel : BindableBase
     {
         private IBlacklistService blacklistService;
+        private IDialogService dialogService;
         private ObservableCollection<BlacklistTrackViewModel> blacklistTracks;
         private bool isBusy;
 
-        public SettingsBlacklistViewModel(IBlacklistService blacklistService)
+        public SettingsBlacklistViewModel(IBlacklistService blacklistService, IDialogService dialogService)
         {
             this.blacklistService = blacklistService;
+            this.dialogService = dialogService;
 
             this.blacklistService.AddedTracksToBacklist += numberOfTracks =>
             {
                 this.GetBlacklistTracksAsync();
             };
 
+            this.RemoveBlacklistTrackCommand = new DelegateCommand<long?>(blacklistTrackId =>
+            {
+                if (this.dialogService.ShowConfirmation(0xe11b, 16, ResourceUtils.GetString("Language_Remove"), ResourceUtils.GetString("Language_Confirm_Remove_Track_From_Blacklist"), ResourceUtils.GetString("Language_Yes"), ResourceUtils.GetString("Language_No")))
+                {
+                    this.RemoveBlacklistTrack(blacklistTrackId.Value);
+                }
+            });
+
             this.GetBlacklistTracksAsync();
         }
+
+        public DelegateCommand<long?> RemoveBlacklistTrackCommand { get; set; }
 
         public ObservableCollection<BlacklistTrackViewModel> BlacklistTracks
         {
@@ -49,7 +63,7 @@ namespace Dopamine.ViewModels.FullPlayer.Settings
             }
         }
 
-        private async void RemoveBlacklistTrackCommand(long blacklistTrackId)
+        private async void RemoveBlacklistTrack(long blacklistTrackId)
         {
             try
             {
